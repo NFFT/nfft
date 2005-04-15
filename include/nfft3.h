9b6afe44a9d8e5f82146916687ae9c11233d89bb
Stefan Kunis <skunis@uos.de>
2005-04-15 11:27:29 +0000

    library header
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/trunk@20 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

---
+++ include/nfft3.h
@@ -0,0 +1,157 @@
+/*! \file nfft3.h
+ *  \brief Header file for the nfft3 library.
+ */
+#ifndef NFFT3_H
+#define NFFT3_H
+
+#include "fftw3.h"
+
+/** 
+ * Constant symbols for precomputation and memory usage
+ */
+#define PRE_PHI_HUT      (1U<< 0)
+#define PRE_PSI          (1U<< 1)
+#define PRE_FULL_PSI     (1U<< 2)
+#define MALLOC_X         (1U<< 3)
+#define MALLOC_F_HAT     (1U<< 4)
+#define MALLOC_F         (1U<< 5)
+#define FFT_OUT_OF_PLACE (1U<< 6)
+
+#define MACRO_MV_PLAN(float_type)			                      \
+  int N_total;                          /**< total number of Fourier coeffs.*/\
+  int M_total;                          /**< total number of samples        */\
+  float_type *f_hat;                    /**< Fourier coefficients           */\
+  float_type *f;                        /**< samples                        */\
+  
+/** @defgroup nfft Group
+ * Direct and fast computation of the
+ * discrete Fourier transform at nonequispaced knots
+ * @{ 
+ */
+typedef struct nfft_plan_ 
+{
+  /** api */
+  MACRO_MV_PLAN(complex);          
+
+  int d;                                /**< dimension, rank                 */
+  int *N;                               /**< cut-off-frequencies             */
+  double *sigma;	                /**< oversampling-factor             */
+  int *n;                               /**< fftw-length = sigma*N           */
+  int n_total;                          /**< total size of fftw              */	
+  int m;                                /**< cut-off parameter in time-domain*/
+  double *b;                            /**< shape parameters                */
+
+  unsigned nfft_flags;                  /**< flags for precomputation, malloc*/
+  unsigned fftw_flags;                  /**< flags for the fftw              */
+
+  double *x;                            /**< nodes (in time/spatial domain)  */
+
+  /** internal*/
+  fftw_plan  my_fftw_plan1;             /**< fftw_plan forward               */
+  fftw_plan  my_fftw_plan2;		/**< fftw_plan backward              */
+
+  double **c_phi_inv;                   /**< precomputed data, matrix D      */
+  double *psi;				/**< precomputed data, matrix B      */
+  int *psi_index_g;                     /**< only for PRE_FULL_PSI           */
+  int *psi_index_f;                     /**< only for PRE_FULL_PSI           */
+
+  complex *g;
+  complex *g_hat;
+  complex *g1;                          /**< input of fftw                   */
+  complex *g2;                          /**< output of fftw                  */
+
+  double *spline_coeffs;          	/**< input for de Boor algorithm, if  
+					     B_SPLINE or SINC_2m is defined  */
+} nfft_plan;
+
+
+/** see formula (1.1), computes
+ * for j=0,...,M-1                                                             
+ *  f[j] = sum_{k in I_N^d} f_hat[k] * exp(-2 (pi) k x[j])
+ */
+void ndft_trafo(nfft_plan *this_plan);
+
+/** see formula (1.1), computes
+ * for j=0,...,M-1                                                             
+ *  f[j] = sum_{k in I_N^d} f_hat[k] * exp(+2 (pi) k x[j])
+ */
+void ndft_conjugated(nfft_plan *this_plan);
+
+/** see formula (1.2), computes
+ * for k in I_N^d
+ *  f_hat[k] = sum_{j=0}^{M-1} f[j] * exp(+2(pi) k x[j])
+ */
+void ndft_adjoint(nfft_plan *this_plan);
+
+/** see formula (1.2), computes
+ * for k in I_N^d
+ *  f_hat[k] = sum_{j=0}^{M-1} f[j] * exp(-2(pi) k x[j])
+ */
+void ndft_transposed(nfft_plan *this_plan);
+
+/** see formula (1.1), computes in a fast and approximative way
+ * for j=0,...,M-1                                                             
+ *  f[j] = sum_{k in I_N^d} f_hat[k] * exp(-2 (pi) k x[j])
+ */
+void nfft_trafo(nfft_plan *this_plan);
+
+/** see formula (1.1), computes in a fast and approximative way
+ * for j=0,...,M-1                                                             
+ *  f[j] = sum_{k in I_N^d} f_hat[k] * exp(+2 (pi) k x[j])
+ */
+void nfft_conjugated(nfft_plan *this_plan);
+
+/** see formula (1.2), computes in a fast and approximative way
+ * for k in I_N^d
+ *  f_hat[k] = sum_{j=0}^{M-1} f[j] * exp(+2(pi) k x[j])
+ */
+void nfft_adjoint(nfft_plan *this_plan);
+
+/** see formula (1.2), computes in a fast and approximative way
+ * for k in I_N^d
+ *  f_hat[k] = sum_{j=0}^{M-1} f[j] * exp(+2(pi) k x[j])
+ */
+void nfft_transposed(nfft_plan *this_plan);
+
+
+
+/** wrapper for nfft_init and d=1
+ */
+void nfft_init_1d(nfft_plan *this_plan, int N1, int M);
+
+/** wrapper for nfft_init and d=2
+ */
+void nfft_init_2d(nfft_plan *this_plan, int N1, int N2, int M);
+
+/** wrapper for nfft_init and d=3
+ */
+void nfft_init_3d(nfft_plan *this_plan, int N1, int N2, int N3, int M);
+
+/** initialisation for direct transform, simple interface
+ */
+void nfft_init(nfft_plan *this_plan, int d, int *N, int M);
+
+/** initialisation for direct transform, specific interface
+ */
+void nfft_init_specific(nfft_plan *this_plan, int d, int *N, int M, int *n,
+	                int m, unsigned nfft_flags, unsigned fftw_flags);
+
+/** precomputes the values psi
+ *  if the PRE_PSI is set the application program has to call this routine
+ *  after setting the nodes this_plan->x
+ */
+void nfft_precompute_psi(nfft_plan *this_plan);
+
+/** precomputes the values psi and their indices in non tensor form
+ *  if the PRE_PSI and PRE_FULL_PSI is set the application program has to call
+ *  this routine after calling nfft_precompute_psi
+ */
+void nfft_full_psi(nfft_plan *this_plan, double eps);
+
+/** finalisation for direct transform
+ */
+void nfft_finalize(nfft_plan *this_plan);
+
+
+#endif
+/* nfft.h */
