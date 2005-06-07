f4449c5af2385312a477b63ce0f9b2744c7dd393
Jens Keiner <jens@nfft.org>
2005-06-07 07:18:21 +0000

    Improved EGM96 example.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/trunk@130 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- obsolete/keiner/examples/egm96/egm96.c
+++ obsolete/keiner/examples/egm96/egm96.c
@@ -42,10 +42,10 @@
 #include "util.h"
 #include "time.h"
 
-#define M 360
-#define N 512
 #define D_PHI 360
 #define D_THETA 180
+#define K_MIN 3
+#define K_MAX 360
 
 /**
  * The main program.
@@ -59,104 +59,137 @@ int main (int argc, char **argv)
 {  
  	complex *f;
   complex **f_hat;
+  complex **f_hat_orig;
   double *x;
   double *phi;
   double *theta;
- 	nfsft_plan plan;
- 	int n,k,d,j,l;
+  nfsft_plan plan;
+  int n,m,k,d,j,l,i, k_min, k_max, d_phi, d_theta, N;
   FILE *file;
+  char filename[100];
   double a,b,c,e;
-  const int D = D_PHI * D_THETA;
+  int D;
   
-  file = fopen("egm96.dat","r");
-  if (file != NULL)
+  if (argc == 1)
   {
-    /** Allocate data structures. */
-    f = (complex*) calloc(D,sizeof(complex));
-    phi = (double*) calloc(D_PHI,sizeof(double));
-    theta = (double*) calloc(D_THETA,sizeof(double));
-    x = (double*) calloc(2*D,sizeof(double));
-    f_hat = (complex**) malloc((2*M+1)*sizeof(complex*));
-    for (n = -M; n <= M; n++)
-    {
-      f_hat[n+M] = (complex*) calloc((N+1),sizeof(complex));
-    }  
+    k_min = K_MIN;
+    k_max = K_MAX; 
+    d_phi = D_PHI;
+    d_theta = D_THETA; 
+  }
+  else if (argc == 5)
+  {
+    sscanf(argv[1],"%d",&k_min);
+    sscanf(argv[2],"%d",&k_max);
+    sscanf(argv[3],"%d",&d_phi);
+    sscanf(argv[4],"%d",&d_theta);
+  }
+  else
+  {
+    fprintf(stderr,"EGM96 - EGM96 example for NFSFT\n");
+    fprintf(stderr,"Usage: EGM96 K_MIN K_MAX D_PHI D_THETA\n");
+    return -1;
+  }    
+  
+  D = d_phi * d_theta;
+  N = 1<<ngpt(k_max);
+  
+  /** Allocate data structures. */
+  f = (complex*) calloc(D,sizeof(complex));
+  phi = (double*) calloc(d_phi,sizeof(double));
+  theta = (double*) calloc(d_theta,sizeof(double));
+  x = (double*) calloc(2*D,sizeof(double));
+  f_hat = (complex**) malloc((2*k_max+1)*sizeof(complex*));
+  f_hat_orig = (complex**) malloc((2*k_max+1)*sizeof(complex*));
+  for (n = -k_max; n <= k_max; n++)
+  {
+    f_hat[n+k_max] = (complex*) calloc((N+1),sizeof(complex));
+    f_hat_orig[n+k_max] = (complex*) calloc((N+1),sizeof(complex));
+  }  
 
-    while (fscanf(file,"%d %d %le %le %le %le\n",&k,&n,&a,&b,&c,&e) != EOF)
+  for (j = 0; j < d_phi; j++)
+  {
+    phi[j] = ((double)j)/d_phi - 0.5;
+  }
+
+  for (l = 0; l < d_theta; l++)
+  {
+    theta[l] = 0.5*((double)l)/(d_theta-1);
+  }
+  
+  d = 0;
+  for (j = 0; j < d_phi; j++)
+  {
+    for (l = 0; l < d_theta; l++)
     {
-		  if (k <= M && k > 5)
-			{
-  		  a *= sqrt(2.0*k+1.0); 
-	  	  b *= sqrt(2.0*k+1.0); 
-        f_hat[n+M][k] = 0.5*(a + I*b);
-        f_hat[-n+M][k] = 0.5*(a - I*b);
-        //fprintf(stderr,"f_hat[%d][%d] = %E; a = %E\n",n,k,f_hat[n+M][k],creal(f_hat[n+M][k]));
-			}
+      x[2*d] = phi[j];
+      x[2*d+1] = theta[l];
+      d++;
     }
-    //f_hat[M][0] = 1.0;
-    fclose(file);
+  }
 
-    /*for (n = -M; n <= M; n++)
+  file = fopen("egm96.dat","r");
+  while (fscanf(file,"%d %d %le %le %le %le\n",&j,&m,&a,&b,&c,&e) != EOF)
+  {
+    if (j <= k_max && j >2)
     {
-      for (k = 3; k <= M ; k++)
-      {
-        f_hat[n+M][k] = 0.0;
-        fprintf(stderr,"f[%d][%d] = %E\n",n,k,creal(f_hat[n+M][k]));
+      a *= sqrt(2.0*j+1.0); 
+      b *= sqrt(2.0*j+1.0); 
+      f_hat_orig[m+k_max][j] = 0.5*(a + I*b);
+      f_hat_orig[-m+k_max][j] = 0.5*(a - I*b);
+    }
+  }
+  fclose(file);
+  
+  nfsft_precompute(k_max,2000);
+
+  plan = nfsft_init(D,k_max,x,f_hat,f,0U);
+  fprintf(stderr,"d_phi = %d, d_theta = %d, D = %d\n",d_phi,d_theta,D);
+  i = 0;
+  for (k = k_min; k <= k_max; k++)
+  {  
+    for (n = k; n <= k; n++)
+    {  
+      fprintf(stderr,"k = %d, n = %d\n",k,n);
+      for (m = -k_max; m <= k_max; m++)
+      {  
+        if (abs(m) <= n)
+        {
+          memcpy(f_hat[m+k_max],f_hat_orig[m+k_max],(k+1)*sizeof(complex));
+          memset(&(f_hat[m+k_max][k+1]),0U,(N-k)*sizeof(complex));
+        }
+        else
+        {
+          memset(f_hat[m+k_max],0U,(N+1)*sizeof(complex));
+        }
       }
-    }*/  
-    //f_hat[4+M][4] = 1E-7; 
-    
-    for (j = 0; j < D_PHI; j++)
-    {
-      phi[j] = ((double)j)/D_PHI - 0.5;
-      fprintf(stderr,"phi[%d] = %f\n",j,phi[j]);
-    }
 
-    for (l = 0; l < D_THETA; l++)
-    {
-      theta[l] = 0.5*((double)l)/(D_THETA-1);
-      fprintf(stderr,"theta[%d] = %f\n",l,theta[l]);
-    }
+      /* Forward transform */
+      nfsft_trafo(plan);
     
-    d = 0;
-    for (j = 0; j < D_PHI; j++)
-    {
-      for (l = 0; l < D_THETA; l++)
-      {
-        x[2*d] = phi[j];
-        x[2*d+1] = theta[l];
-        d++;
-      }
-    }
-    
-    nfsft_precompute(M,2000);
-
-    /* Forward transform */
-    plan = nfsft_init(D,M,x,f_hat,f,0U);
-    nfsft_trafo(plan);
-    nfsft_finalize(plan);
-    
-    file = fopen("egm96f.dat","w");
-    if (file != NULL)
-    {
+      sprintf(filename,"egm96%05d.dat",i);
+      file = fopen(filename,"w");
       for (d = 0; d < D; d++)
       {
-        //fprintf(file,"%.16f %.16f %.16f %.16f\n",x[2*d],x[2*d+1],creal(f[d]),cimag(f[d]));
         fprintf(file,"%.16f\n",cabs(f[d]));
       }
       fclose(file);
+      i++;
     }
-
-    for (n = -M; n <= M; n++)
-    {
-      free(f_hat[n+M]);
-    }   
-    free(f_hat);
-    free(x);
-    free(theta);
-    free(phi);
-    free(f);
   }  
-  	
+  nfsft_finalize(plan);
+    
+  for (n = -k_max; n <= k_max; n++)
+  {
+    free(f_hat[n+k_max]);
+    free(f_hat_orig[n+k_max]);
+  }   
+  free(f_hat);
+  free(f_hat_orig);
+  free(x);
+  free(theta);
+  free(phi);
+  free(f);
+  
   return EXIT_SUCCESS;
 }
--- obsolete/keiner/examples/egm96/egm96.m
+++ obsolete/keiner/examples/egm96/egm96.m
@@ -1,57 +1,13 @@
-f = fopen('egm96f.dat','r');
-topo = fscanf(f,'%f\n',[180,360]);
-fclose(f);
-
-%load('topo.mat','topo','topomap1');
-whos topo topomap1
-figure
-imagesc(topo)
-colorbar
-return
-
-%% Contour plot
-% CONTOUR creates a contour plot of the Earth from this data.  Here, we are
-% producing a contour based on points on the map that have zero altitude.  What
-% you thus see is an outline of the continents.
-
-contour(0:359,-89:90,topo,[0 0],'b')
-axis equal
-box on
-set(gca,'XLim',[0 360],'YLim',[-90 90], ... 
-    'XTick',[0 60 120 180 240 300 360], ...
-    'Ytick',[-90 -60 -30 0 30 60 90]);
-
-%% 2-D Image plot
-% IMAGE creates a 2-D image plot from the data in topo and topomap1.  You can
-% see the elevation information in this figure.  Here, altitudes correspond to
-% shades of green, while depths (below sea level) correspond to shades of blue.
-
-hold on
-image([0 360],[-90 90],topo,'CDataMapping', 'scaled');
-colormap(topomap1);
-
-%% 3-D plot   
-% The globe!
-%
-% The SPHERE function returns x,y,z data that are points on the surface of a
-% sphere (50 points in this case).  Observe the altitude data in topo mapped
-% onto the coordinates of the sphere contained in x, y and z.  Two light sources
-% illuminate the globe.
-
-[x,y,z] = sphere(50);
-
-cla reset
-axis square off
-props.AmbientStrength = 0.1;
-props.DiffuseStrength = 1;
-props.SpecularColorReflectance = .5; 
-props.SpecularExponent = 20;
-props.SpecularStrength = 1;
-props.FaceColor= 'texture';
-props.EdgeColor = 'none';
-props.FaceLighting = 'phong';
-props.Cdata = topo;
-surface(x,y,z,props);
-light('position',[-1 0 1]);
-light('position',[-1.5 0.5 -0.5], 'color', [.6 .2 .2]);
-view(3)
+k_min = 0
+k_max = 7
+d_phi = 720
+d_theta = 360
+for k=k_min:k_max
+  filename = sprintf('egm96%05d.dat',k);
+  f = fopen(filename,'r');
+  topo = fscanf(f,'%f\n',[d_theta,d_phi]);
+  imagesc(topo)
+  fclose(f);
+  F(k+1) = getframe(gcf)
+end
+movie(F)
