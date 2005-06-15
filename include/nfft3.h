f2e3b975fef3cf92331fa2d00bbda374089c5cfb
Stefan Kunis <skunis@uos.de>
2005-06-15 16:45:41 +0000

    Header contains solver and nsfft
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/trunk@141 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- include/nfft3.h
+++ include/nfft3.h
@@ -5,7 +5,7 @@
 #define NFFT3_H
 
 #include <complex.h>
-#include "fftw3.h"
+#include <fftw3.h>
 
 /** 
  * Constant symbols for precomputation and memory usage
@@ -18,8 +18,11 @@
 #define MALLOC_F_HAT     (1U<< 5)
 #define MALLOC_F         (1U<< 6)
 #define FFT_OUT_OF_PLACE (1U<< 7)
-#define MALLOC_V         (1U<< 8)
-#define FFTW_INIT        (1U<< 9)
+#define FFTW_INIT        (1U<< 8)
+
+#define MALLOC_V         (1U<< 9)
+
+#define SNDFT            (1U<< 10)
 
 #define MACRO_MV_PLAN(float_type)			                      \
   int N_total;                          /**< total number of Fourier coeffs.*/\
@@ -41,7 +44,7 @@ typedef struct nfft_plan_
   int *N;                               /**< cut-off-frequencies             */
   double *sigma;	                /**< oversampling-factor             */
   int *n;                               /**< fftw-length = sigma*N           */
-  int n_total;                          /**< total size of fftw              */	
+  int n_total;                          /**< total size of fftw              */
   int m;                                /**< cut-off parameter in time-domain*/
   double *b;                            /**< shape parameters                */
   int K;                                /**< number of precomp. uniform psi  */
@@ -67,8 +70,6 @@ typedef struct nfft_plan_
 
   double *spline_coeffs;          	/**< input for de Boor algorithm, if  
 					     B_SPLINE or SINC_2m is defined  */
-
-  int size_psi;                         /**< if PRE_FULL_PSI is set          */
 } nfft_plan;
 
 
@@ -170,51 +171,44 @@ void nfft_finalize(nfft_plan *ths);
  */
 
  
- 
- 
- 
- 
 /** @defgroup nnfft Group
  * Direct and fast computation of the
  * discrete Fourier transform at nonequispaced knots in time- and fourier-domain
  * @{ 
  */
- typedef struct nnfft_plan_ 
+typedef struct nnfft_plan_ 
 {
   /** api */
   MACRO_MV_PLAN(complex);
 
-  int d;                                /**< dimension, rank                  */
-  double *sigma;                        /**< oversampling-factor 1            */
-  double *a;                            /**< 1 + 2*m1/N1                      */ 
-  int *N;                               /**< cut-off-frequencies              */
+  int d;                                /**< dimension, rank                 */
+  double *sigma;                        /**< oversampling-factor 1           */
+  double *a;                            /**< 1 + 2*m1/N1                     */
+  int *N;                               /**< cut-off-frequencies             */
   int *N1;
   int *aN1;
-  int m;                                /**< cut-off parameter in time-domain */
-  double *b;                            /**< shape parameters                 */
+  int m;                                /**< cut-off parameter in time-domain*/
+  double *b;                            /**< shape parameters                */
   int K;                                /**< number of precomp. uniform psi  */
 
   int aN1_total;
   
-  nfft_plan *direct_plan;               /**< plan for the nfft                */
-  unsigned nnfft_flags;                 /**< flags for precomputation, malloc */
-
-  int *n;                               /**<  = a*N                           */
+  nfft_plan *direct_plan;               /**< plan for the nfft               */
+  unsigned nnfft_flags;                 /**< flags for precomputation, malloc*/
+  int *n;                               /**<  = a*N                          */
   
-  double *x;                            /**< nodes (in time/spatial domain)   */
-  
-  double *v;                            /**< nodes (in fourier domain)   */
+  double *x;                            /**< nodes (in time/spatial domain)  */
+  double *v;                            /**< nodes (in fourier domain)       */
 
-  double *c_phi_inv;                    /**< precomputed data, matrix D       */
-  double *psi;                          /**< precomputed data, matrix B       */
-  int size_psi;                         /**< only for thin B                  */
-  int *psi_index_g;                     /**< only for thin B                  */
-  int *psi_index_f;                     /**< only for thin B                  */
-
+  double *c_phi_inv;                    /**< precomputed data, matrix D      */
+  double *psi;                          /**< precomputed data, matrix B      */
+  int size_psi;                         /**< only for thin B                 */
+  int *psi_index_g;                     /**< only for thin B                 */
+  int *psi_index_f;                     /**< only for thin B                 */
   complex *F;
 
   double *spline_coeffs;                /**< input for de Boor algorithm, if  
-                                             B_SPLINE or SINC_2m is defined   */
+                                             B_SPLINE or SINC_2m is defined  */
 } nnfft_plan;
 
 
@@ -228,8 +222,8 @@ void nndft_transposed(nnfft_plan *ths_plan);
 
 void nnfft_init(nnfft_plan *ths_plan, int d, int N_total, int M_total, int *N);
 
-void nnfft_init_guru(nnfft_plan *ths_plan, int d, int N_total, int M_total, int *N, int *N1,
-                        int m, unsigned nnfft_flags);
+void nnfft_init_guru(nnfft_plan *ths_plan, int d, int N_total, int M_total,
+                     int *N, int *N1, int m, unsigned nnfft_flags);
                         
 void nnfft_finalize(nnfft_plan *ths_plan);
 
@@ -251,6 +245,126 @@ void nnfft_precompute_phi_hut(nnfft_plan *ths_plan);
  
 /* @} 
  */
+
+/** @defgroup nsfft Group
+ * @{ 
+ */
+typedef struct nsfft_plan_ 
+{
+  MACRO_MV_PLAN(complex);
+
+  int d;                                /**< d=2,3                           */
+
+  int J;                      
+  int sigma;
+
+  unsigned flags;
+
+  int *index_sparse_to_full;            /**< overflow for d=3, J=9!          */
+
+  int r_act_nfft_plan;
+  nfft_plan *act_nfft_plan;
+  nfft_plan *center_nfft_plan;
+
+  fftw_plan* set_fftw_plan1;
+
+  nfft_plan *set_nfft_plan_1d;
+  nfft_plan *set_nfft_plan_2d;          /**< d=3                             */
+
+  double *x_transposed;                 /**< d=2                             */
+  double *x_102,*x_201,*x_120,*x_021;   /**< d=3                             */
+  
+} nsfft_plan;
+
+void nsdft_trafo(nsfft_plan *ths);
+void nsdft_adjoint(nsfft_plan *ths); /*TODO!--------------------------------------------------------*/
+
+void nsfft_trafo(nsfft_plan *ths);
+void nsfft_adjoint(nsfft_plan *ths); /*TODO!--------------------------------------------------------*/
+
+/** Copy coefficients and knots from ths to ths_nfft */
+void nsfft_cp(nsfft_plan *ths, nfft_plan *ths_nfft);
+void nsfft_init_random_nodes_coeffs(nsfft_plan *ths);
+
+void nsfft_init(nsfft_plan *ths, int d, int J, int M, int m, unsigned flags);
+void nsfft_finalize(nsfft_plan *ths);
+
+/* @} 
+ */
+
+/** @defgroup solver Group
+ * @{ 
+ */
+
+/** 
+ * Constant symbols for precomputation and memory usage (inverse problem)
+ */
+#define LANDWEBER           	(1U<< 0)
+#define STEEPEST_DESCENT    	(1U<< 1)
+#define CGNR                    (1U<< 2)
+#define CGNE 		    	(1U<< 3)
+#define NORMS_FOR_LANDWEBER 	(1U<< 4)
+#define PRECOMPUTE_WEIGHT   	(1U<< 5)
+#define PRECOMPUTE_DAMP     	(1U<< 6)
+/** will come in again, as method by their own, together with some new method for
+    the minimal seminorm interpolation
+    #define REGULARIZE_CGNR     	(1U<< 7)
+    #define REGULARIZE_CGNR_R_HAT   (1U<< 8)
+ double *r_hat;                        regularisation weights           
+  double lambda;                        regularisation parameter         
+*/
+
+/** 
+ * \brief function mangling macro
+ *  
+ * \arg MV matrix vector multiplication type
+ * \arg FLT float type, i.e., double resp. complex
+ * \arg name name of the function
+ * \arg ...  argument list of the function
+ */
+#define F(MV, FLT, name, ...) void i ## MV ## _ ## name(__VA_ARGS__)
+
+#define MACRO_SOLVER_PLAN(MV, FLT)	          		              \
+typedef struct i ## MV ## _plan_				              \
+{									      \
+  MV ## _plan *mv;			/**< matrix vector multiplication   */\
+  unsigned flags;			/**< iteration type, ...            */\
+									      \
+  double *w;                         	/**< weighting factors              */\
+  double *w_hat;                     	/**< damping factors                */\
+									      \
+  FLT *y;                	        /**< right hand side, samples       */\
+									      \
+  FLT *f_hat_iter;		        /**< iterative solution             */\
+									      \
+  FLT *r_iter;			        /**< iterated residual vector       */\
+  FLT *z_hat_iter;             	        /**< residual vector of normal eq.1 */\
+  FLT *p_hat_iter;             	        /**< search direction               */\
+  FLT *v_iter;                 	        /**< residual vector update         */\
+									      \
+  double alpha_iter;                    /**< step size for search direction */\
+  double beta_iter;                     /**< step size for search correction*/\
+									      \
+  double dot_r_iter;                    /**< dotproductc{_w}(r_iter)        */\
+  double dot_r_iter_old;                /**< old dotproductc{_w}(r_iter)    */\
+  double dot_z_hat_iter;                /**< dotproductc{_w}(z_hat_iter)    */\
+  double dot_z_hat_iter_old;            /**< old dotproductc{_w}(z_hat_iter)*/\
+  double dot_p_hat_iter;                /**< dotproductc{_w}(p_hat_iter)    */\
+  double dot_v_iter;                    /**< dotproductc{_w}(v_iter)        */\
+} i ## MV ## _plan;                                                           \
+									      \
+F(MV, FLT, init,	  i ## MV ## _plan *ths, MV ## _plan *mv);            \
+F(MV, FLT, init_advanced, i ## MV ## _plan *ths, MV ## _plan *mv,             \
+		     	  unsigned i ## MV ## _flags);		              \
+F(MV, FLT, before_loop,   i ## MV ## _plan *ths);                             \
+F(MV, FLT, loop_one_step, i ## MV ## _plan *ths);                             \
+F(MV, FLT, finalize,      i ## MV ## _plan *ths);                             \
+
+
+MACRO_SOLVER_PLAN(nfft, complex)
+MACRO_SOLVER_PLAN(nnfft, complex)
+/** @} 
+ */
  
 #endif
-/* nfft.h */
+/* nfft3.h */
