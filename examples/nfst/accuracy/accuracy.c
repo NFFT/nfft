06216f07ef5de19cefe482c1b7628a22a44fa9ee
ana <ana>
2005-09-23 16:09:14 +0000

    two tiny, pretty identical examples
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/trunk@324 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

---
+++ examples/nfst/accuracy/accuracy.c
@@ -0,0 +1,102 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "util.h"
+#include "nfft3.h"
+
+void accuracy(int d, int M, int m1, int m2)
+{
+  int j,k,t,m;                         
+  nfst_plan my_plan;                   
+
+  double *slow;
+  double *my_f_hat;
+  double *my_x;
+  
+  int N[d], n[d];
+
+  srand( 27031950);
+
+  for( t = 0; t < d; t++) {
+    N[t] = (1<<(12/d - 1)<<1);
+    n[t] = 2 * next_power_of_2( N[t]) - 1;
+    
+    printf( "N[%d]=%d\n", t, N[t]);
+  }
+
+  nfst_init_guru(&my_plan, d, N, M, n, 6,
+     	  MALLOC_X| MALLOC_F_HAT| MALLOC_F|
+          FFTW_INIT| FFT_OUT_OF_PLACE, 
+          FFTW_ESTIMATE| FFTW_DESTROY_INPUT);
+
+  slow = (double*)fftw_malloc( M * sizeof( double));
+  my_x = (double*)fftw_malloc( d * M * sizeof( double));
+  my_f_hat = (double*)fftw_malloc( my_plan.N_total * sizeof( double));
+
+
+  /** init pseudo random nodes */
+  for( j = 0; j < d*M; j++)
+    my_x[j] = 0.5 * ((double)rand()) / RAND_MAX;
+
+  /** init pseudo random Fourier coefficients and show them */
+  for( k = 0; k < my_plan.N_total; k++)
+    my_f_hat[k] = (double)rand() / RAND_MAX;
+ 
+  for( j = 0; j < d*M; j++)
+    my_plan.x[j] = my_x[j];
+
+  /** init pseudo random Fourier coefficients and show them */
+  for( k = 0; k < my_plan.N_total; k++)
+    my_plan.f_hat[k] = my_f_hat[k];
+   
+  /** direct trafo and show the result */
+  ndst_trafo( &my_plan);
+
+  for( j = 0; j < M; j++)
+    slow[j] = my_plan.f[j];
+
+  nfst_finalize( &my_plan);
+
+    
+  for( m = m1; m < m2+1; m++) {
+
+    nfst_init_guru(&my_plan, d, N, M, n, m,
+			 PRE_PHI_HUT| PRE_PSI| MALLOC_X| MALLOC_F_HAT| MALLOC_F|
+			 FFTW_INIT| FFT_OUT_OF_PLACE, 
+			 FFTW_ESTIMATE| FFTW_DESTROY_INPUT);
+      
+      /** init pseudo random nodes */
+    for( j = 0; j < d * M; j++)
+      my_plan.x[j] = my_x[j];
+      
+    /** precompute psi, the entries of the matrix B */
+    if( my_plan.nfst_flags & PRE_PSI)
+      nfst_precompute_psi( &my_plan);
+      
+    /** init pseudo random Fourier coefficients and show them */
+    for( k = 0; k < my_plan.N_total; k++)
+      my_plan.f_hat[k] = my_f_hat[k];
+      
+    /** approx. trafo and show the result */
+    nfst_trafo( &my_plan);
+
+    fprintf( stderr, "%d  %e, %e\n", m,
+      error_l_2_double( slow, my_plan.f, M),
+      error_l_infty_1_double( slow, my_plan.f, M, my_plan.f_hat, my_plan.N_total));
+      
+    /** finalize the one dimensional plan */
+    nfst_finalize(&my_plan);
+  }
+
+  fftw_free( slow);
+  fftw_free( my_x);
+  fftw_free( my_f_hat);
+}
+
+int main(int argc, char **argv) { 
+
+  accuracy( atoi( argv[1]), atoi( argv[2]), atoi( argv[3]), atoi( argv[4]));
+
+  return EXIT_SUCCESS;
+}
