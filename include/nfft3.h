05e772c9cff310b7921cf552ee9608ede658fd00
Jens Keiner <jens@nfft.org>
2005-09-26 12:14:47 +0000

    Continued fast summation paper. Minor changes to nfct.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/trunk@327 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- include/nfft3.h
+++ include/nfft3.h
@@ -4,8 +4,23 @@
 #ifndef NFFT3_H
 #define NFFT3_H
 
+/** Include header for C99 complex datatype. */
 #include <complex.h>
+/** Include header for FFTW3 library. */
 #include <fftw3.h>
+
+/** Macros for public members inherited by all plan structures */
+#define MACRO_MV_PLAN(float_type)                                           \
+int N_total;                          /**< total number of Fourier coeffs.*/\
+int M_total;                          /**< total number of samples        */\
+float_type *f_hat;                    /**< Fourier coefficients           */\
+float_type *f;                        /**< samples                        */\
+ 
+/** @defgroup nfft Group
+ * Direct and fast computation of the
+ * discrete Fourier transform at nonequispaced knots
+ * @{ 
+ */
 
 /** 
  * Constant symbols for precomputation and memory usage
@@ -24,17 +39,6 @@
 
 #define SNDFT            (1U<< 10)
 
-#define MACRO_MV_PLAN(float_type)			                      \
-  int N_total;                          /**< total number of Fourier coeffs.*/\
-  int M_total;                          /**< total number of samples        */\
-  float_type *f_hat;                    /**< Fourier coefficients           */\
-  float_type *f;                        /**< samples                        */\
-  
-/** @defgroup nfft Group
- * Direct and fast computation of the
- * discrete Fourier transform at nonequispaced knots
- * @{ 
- */
 typedef struct nfft_plan_ 
 {
   /** api */
@@ -42,7 +46,7 @@ typedef struct nfft_plan_
 
   int d;                                /**< dimension, rank                 */
   int *N;                               /**< cut-off-frequencies             */
-  double *sigma;	                /**< oversampling-factor             */
+  double *sigma;	                       /**< oversampling-factor             */
   int *n;                               /**< fftw-length = sigma*N           */
   int n_total;                          /**< total size of fftw              */
   int m;                                /**< cut-off parameter in time-domain*/
@@ -56,10 +60,10 @@ typedef struct nfft_plan_
 
   /** internal*/
   fftw_plan  my_fftw_plan1;             /**< fftw_plan forward               */
-  fftw_plan  my_fftw_plan2;		/**< fftw_plan backward              */
+  fftw_plan  my_fftw_plan2;		           /**< fftw_plan backward              */
 
   double **c_phi_inv;                   /**< precomputed data, matrix D      */
-  double *psi;				/**< precomputed data, matrix B      */
+  double *psi;				                      /**< precomputed data, matrix B      */
   int *psi_index_g;                     /**< only for PRE_FULL_PSI           */
   int *psi_index_f;                     /**< only for PRE_FULL_PSI           */
 
@@ -68,8 +72,8 @@ typedef struct nfft_plan_
   complex *g1;                          /**< input of fftw                   */
   complex *g2;                          /**< output of fftw                  */
 
-  double *spline_coeffs;          	/**< input for de Boor algorithm, if  
-					     B_SPLINE or SINC_2m is defined  */
+  double *spline_coeffs;          	     /**< input for de Boor algorithm, if  
+					                                        B_SPLINE or SINC_2m is defined  */
 } nfft_plan;
 
 
@@ -1116,24 +1120,25 @@ struct texture_plan {
 /* @}
  */
 
-/** @defgroup nfsft NFSFT
- * Direct and fast computation of the discrete spherical Fourier transform at 
- * nonequispaced nodes in time and Fourier domain
+/** 
+ * @defgroup nfsft NFSFT
+ * Nonuniform fast spherical Fourier transforms (NFSFT)
  * @{ 
  */
 
-
-/* Plan flags */
+/* Planner flags */
 
 /** 
- * If set, all computations for the created plan are carried out with spherical 
- * harmonics \f$Y_k^n\f$ normalized with respect to the 
- * \f$\text{L}^2\left(\mathbb{S}^2\right)\f$ standard inner product
- * \f[ 
- *   <f,g>_{\text{L}^2\mathbb{S}^2} = \int_{0}^{\pi} \int_{-\pi}^{\pi} 
- *   f(\vartheta,\varphi) \overline{g(\vartheta,\varphi)} \; d\varphi \; 
- *   d\vartheta.
- * \f]
+ * By default, all computations are performed with respect to the semi-
+ * normalized spherical surface functions $Y_k^n$ defined by
+ * \[
+ *   Y_k^n = P_k^{|n|}(\cos\vartheta) e^{i n \varphi}.
+ * \]
+ * If this flag is set, all computations are carried out using \f$L_2\f$-
+ * normalized spherical surface functions \f$Y_k^n\f$ defined by
+ * \[
+ *   Y_k^n = \sqrt{\frac{2k+1}{4\pi}} P_k^{|n|}(\cos\vartheta) e^{i n \varphi}.
+ * \]  
  * 
  * \see nfsft_init
  * \author Jens Keiner
@@ -1141,70 +1146,72 @@ struct texture_plan {
 #define NFSFT_NORMALIZED (1U<<0)
 
 /**
- * If set, the direct NDFT algorithm will be used internally instead of the 
- * approximative NFFT algorithm.
+ * If this flag is set, the direct NDFT algorithm will be used internally 
+ * instead of the fast approximative NFFT algorithm.
  *
  * \see nfsft_init
  * \author Jens Keiner
  */
-#define NFSFT_USE_NDFT   (1<<0)
-
+#define NFSFT_USE_NDFT   (1U<<0)
 
 /* Precomputation flags */
 
 /**
- * If set, the direct algorithms do not work. Setting this flag saves some 
- * memory during precomputation.
+ * If this flag is set, the algorithms direct NDSFT and adjoint direct NDSFT do
+ * not work. Setting this flag saves some memory for precomputed data.
  * 
  * \see nfsft_precompute
+ * \see ndsft_trafo
+ * \see ndsft_adjoint
  * \author Jens Keiner
  */
 #define NFSFT_NO_DIRECT  (1U<<0)
 
 /**
- * If set, The fast algorithms only work in a certain bandwidth window. If 
- * \f$N\f$ is the power of two up to which precomputation is performed, only 
- * transformations for bandwidth \f$M\f$ with \f$N/2 < M \le N\f$ will work.
+ * If this flag is set, the fast algorithms NFSFT and adjoint NFSFT only work 
+ * in a defined bandwidth window. If \f$N\f$ is the power of two up to which 
+ * precomputation is performed, only fast transformations for bandwidth 
+ * \f$M\f$ with \f$N/2 < M \le N\f$ will work. The slow algorithms direct NDSFT
+ * and adjoint direct NDSFT are unaffected. Using this flag saves memory for
+ * precomputed data.
  *
  * \see nfsft_precompute
+ * \see nfsft_trafo
+ * \see nfsft_adjoint
  * \author Jens Keiner
  */
-#define NFSFT_BW_WINDOW  (1<<1)
+#define NFSFT_BW_WINDOW  (1U<<1)
 
 /** Structure for a transform plan */
-struct nfsft_plan_
+typedef struct nfsft_plan_
 {
+  /** Inherited public members */
   MACRO_MV_PLAN(complex);          
 
-  /* API */
-  int M;                                /**< The bandwidth \f$M\f$           */
-  int D;                                /**< The number of nodes \f$D\f$     */  
+  /* Public members */
+  int N;                                 /**< The bandwidth \f$N\f$           */
   double *x;                            /**< The nodes \f$\mathcal{X} = 
                                              \left(\xi_d\right)_{d=0}
                                              ^{D-1}\f$                       */
   
-  /* Internal */
-  int N;                                /**< Next greater power of two with
-                                             respect to M                    */
-  int t;                                /**< The logaritm of N with respect 
+  /* Private members */
+  int NP2;                              /**< Next greater power of two with
+                                             respect to \f$N\f|              */
+  int t;                                /**< The logaritm of NP2 with respect 
                                              to the basis 2                  */  
-
-  unsigned int flags;                   /**< The flags                       */
-
+  unsigned int flags;                   /**< The planner flags               */
   nfft_plan plan_nfft;                  /**< The internal NFFT plan          */
-} nfsft_plan_s;
-
-/** Typedef for transform plans */
-typedef struct nfsft_plan_s *nfsft_plan;
-
+} nfsft_plan;
 
 /**
  * Creates a transform plan.
  *
- * \arg M The bandwidth \f$M\f$
- * \arg D The number of nodes \f$D\f$
+ * \arg N The bandwidth \f$N\f$
+ * \arg M The number of nodes \f$M\f$
  * \arg f_hat The spherical Fourier coefficients 
- *      \f$\left(a_k^n\right)_{(k,n) \in \mathcal{I}^M}\f$
+ *            \f$\left(a_k^n\right)_{(k,n) \in \mathcal{I}^N}\f$ in order-major
+ *            order. The coefficients must be aligned as follows:
+ *            \todo Continue this comment
  * \arg x The nodes \f$\left(\mathbf{\xi}_d\right)_{d = 0}^{D - 1}\f$
  * \arg f The function values \f$\left(f_d\right)_{d = 0}^{D - 1}\f$
  *
@@ -1212,7 +1219,13 @@ typedef struct nfsft_plan_s *nfsft_plan;
  *
  * \author Jens Keiner
  */
-nfsft_plan nfsft_init(int M, int D, complex *f_hat, double *x, complex *f);
+void nfsft_init(
+                nfsft_plan *plan, 
+                int M, 
+                int D, 
+                complex *f_hat, 
+                double *x, 
+                complex *f);
 
 /**
  * Creates a transform plan.
@@ -1229,8 +1242,8 @@ nfsft_plan nfsft_init(int M, int D, complex *f_hat, double *x, complex *f);
  *
  * \author Jens Keiner
  */
-nfsft_plan nfsft_init_advanced(int M, int D, complex *f_hat, double *x, 
-                               complex *f, unsigned int nfsft_flags);
+void nfsft_init_advanced(nfsft_plan* plan, int M, int D, complex *f_hat, 
+                         double *x, complex *f, unsigned int nfsft_flags);
 
 /**
  * Creates a transform plan.
@@ -1248,8 +1261,8 @@ nfsft_plan nfsft_init_advanced(int M, int D, complex *f_hat, double *x,
  *
  * \author Jens Keiner
  */
-nfsft_plan nfsft_init_guru(int M, int D, complex *f_hat, double *x, complex *f, 
-                           unsigned int flags, int nfft_cutoff);
+void nfsft_init_guru(nfsft_plan* plan, int M, int D, complex *f_hat, double *x,
+                     complex *f, unsigned int flags, int nfft_cutoff);
 
 /**
  * Performes precomputation up to the next power of two with respect to a given 
@@ -1284,7 +1297,7 @@ void nfsft_forget();
  *
  * \author Jens Keiner
  */
-void ndsft_trafo(nfsft_plan plan);
+void ndsft_trafo(nfsft_plan* plan);
 
 /**
  * Executes a direct adjoint NDSFT, i.e. computes for \f$(k,n) \in 
@@ -1298,7 +1311,7 @@ void ndsft_trafo(nfsft_plan plan);
  *
  * \author Jens Keiner
  */
-void ndsft_adjoint(nfsft_plan plan);
+void ndsft_adjoint(nfsft_plan* plan);
 
 /**
  * Executes a NFSFT, i.e. computes for \f$d = 0,\ldots,D-1\f$
@@ -1312,7 +1325,7 @@ void ndsft_adjoint(nfsft_plan plan);
  *
  * \author Jens Keiner
  */
-void nfsft_trafo(nfsft_plan plan);
+void nfsft_trafo(nfsft_plan* plan);
 
 /**
  * Executes an adjoint NFSFT, i.e. computes for \f$(k,n) \in 
@@ -1326,7 +1339,7 @@ void nfsft_trafo(nfsft_plan plan);
  *
  * \author Jens Keiner
  */
-void nfsft_adjoint(nfsft_plan plan);
+void nfsft_adjoint(nfsft_plan* plan);
 
 /**
  * Destroys a plan.
