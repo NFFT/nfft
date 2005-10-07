c528fa00813a2df85283f6efa16fd6b2703d7b45
Jens Keiner <jens@nfft.org>
2005-10-07 20:34:38 +0000

    Continued migration to new directory structure.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@349 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- doc/paper/fastsumS2/images/my_legendre.m
+++
@@ -1,8 +0,0 @@
-function p=my_legendre(k,x)
-
-pp=legendre(k,x);
-if(k==0)
-  p=pp;
-else
-  p=squeeze(pp(1,:,:));
-end;
--- doc/paper/stability/images/sobolev_weight.m
+++
@@ -1,7 +0,0 @@
-function g=sobolev_weight(z,alpha,beta,gamma)
-
-if(beta==0)
-  g=(gamma+abs(z).^(2*alpha)).^(-1);
-else
-  g=(gamma+abs(z).^(2*alpha)).^(-1) .* (1/4-z.^2).^beta * 4^beta;
-end
