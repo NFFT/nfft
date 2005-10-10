ac038a72be2a4debec1167d20ede3a9b5d63275c
ana <ana>
2005-10-10 22:36:29 +0000

    added a view things fr_sample and func_rec_2d vol87.dat holds glacier data
    /skl
    
    
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@364 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- examples/kernel/nfct/func_rec_2d/fr_sample.m
+++ examples/kernel/nfct/func_rec_2d/fr_sample.m
@@ -1,20 +1,52 @@
-function f = fr_sample( M, min_x, min_y, max_x, max_y, fun, shift_x, shift_y);
+function f = fr_sample( M, min_x, min_y, max_x, max_y, fun, shift_x, shift_y, kind);
 
-vx = ( max_x - min_x) * rand( M, 1);
-vy = ( max_y - min_y) * rand( M, 1);
+if ( kind == ['rand'])
+  % random:
+  vx = ( max_x - min_x) * rand( M, 1);
+  vy = ( max_y - min_y) * rand( M, 1);
+  this_M = M;
+end
 
-ffile = fopen( 'input.dat', 'w');
+if ( kind == ['grid'])
+  % grid:
+  M = floor( sqrt( M));
+  vy_ = [ min_y + shift_y:( max_y - min_y) / (M-1):max_y + shift_y];
+  vy  = [];
+  for i = 1:M;
+    vy  = [ vy_, vy];
+  end;
+  vx_ = ones( 1, M)/(M-1);
+  vx  = [];
+  for i = 1:M;
+    vx  = [ vx, shift_x + min_x + ( ( max_x - min_x) * ( i-1) * vx_)];
+  end;
+  this_M = M * M;
+end;
 
-for i = 1:M;
+if ( kind == ['data'])
+  this_M = M;
+  DF = load( fun);
+end
 
-  x = vx( i) - shift_x;
-  y = vy( i) - shift_y;
+fifile = fopen( 'input.dat', 'w');
+
+for i = 1:this_M;
+
+  if ( kind == ['data'])
+    
+    x = DF( i, 1);
+    y = DF( i, 2);
+    f = DF( i, 3);
+  else
   
-  f = feval( fun, x, y);
-
-  fprintf( ffile, '%f %f %f\n', vx( i), vy( i), f);
+    x = vx( i);
+    y = vy( i);
+    f = feval( fun, x + shift_x, y + shift_y);
+  end 
+ 
+  fprintf( fifile, '%f %f %f\n', x, y, f);
 
 end;
 
-fclose(ffile);
+fclose( fifile);
 
--- examples/kernel/nfct/func_rec_2d/func_rec_2d.m
+++ examples/kernel/nfct/func_rec_2d/func_rec_2d.m
@@ -2,7 +2,7 @@
 %rec_M: reconstruct at rec_M * rec_M grid
 function func_rec_2d( M, rec_M)
 
-border_eps=0.0;
+border_eps=0.2;
 
 % damping weights options
 mu=1.2;
@@ -13,29 +13,49 @@ N=256;
 % for a number of runs use [i0 i1 ... in]
 % output is written into seperate files
 %iter=[10 20 30 40];
-iter=[40];
+iter=[40];%10 20 30 40];
 
 %misc options (0=false, 1=true)
 wait_for_key=0;                  % have matlab wait for input after each run 
 show_fig=1;                      % show 3d-figure after computation
-make_eps_file=1;                 % create eps-file of figure, show_fig must be set
+make_eps_file=0;                 % create eps-file of figure, show_fig must be set
 show_axis=0;                     % have axes in figure
 
 % compile func_rec-2d if neseccary
 !make
 
 
-% create M random 'x y f(x,y)' pairs
-% write results to 'input.dat'
-% evaluate function 'func.m' at random nodes
-% between min_x:max_x and min_y:max_y
-% shift x and y by shift_{x,y} (meaning func(x-shift_x, y-shift_y))
-% example: fr_sample( M, min_x, max_x, min_y, max_y, 'func', shift_x, shift_y);
-% (shift function into first quadrant)
+% create datafile of the form 'x y f(x,y)' pairs
+% synopsis: fr_sample( M, min_x, max_x, min_y, max_y, 'func', shift_x, shift_y, 'kind');
+%
+%
+% writes results to 'input.dat'
+%
+% 'data': -only M is significant, all other arguments
+%          will be ignored. this is not nice but will do at the time..
+%         -M is then the number of lines in datafile that must
+%          satisfy the format above.
 
-% for now there are "sinx_xysqr: sinc( x^2 + y^2)
-%                   "franke:     franke( x ,y)
-fr_sample( M, -3.0, -3.0, 3.0, 3.0, 'sinc_xysqr', 3.0, 3.0);
+% 'rand': -evaluate function 'func.m' at random nodes in the
+%          interval [min_x,max_x] x [min_y:max_y]
+%         -shift x and y by shift_{x,y} (meaning func(x+shift_x, y+shift_y))
+
+% 'grid': -evaluate function 'func.m' at sqrt(M)*sqrt(M) grid in the
+%          interval [min_x,max_x] x [min_y:max_y]
+%         -shift x and y by shift_{x,y} (meaning func(x+shift_x, y+shift_y))
+%         -M := sqrt(M)^2: this means, if M has no integer square root it will
+%          be "adjusted"
+
+% functions:
+% for now there are "sinx_xysqr": sinc( x^2 + y^2)
+%                   "franke":     franke( x ,y)
+%
+% datafiles:
+%                   "vol87.dat"
+%
+fr_sample( M, -2.0, -2.0, 2.0, 2.0, 'sinc_xysqr', -2.0, -2.0, 'rand');
+%fr_sample( M, -2.0, -2.0, 2.0, 2.0, 'sinc_xysqr', +2.0, +2.0, 'grid');
+%fr_sample( M, 0, 0, 0, 0, 'vol87.dat', 0, 0, 'data');
 
 
 for l = iter
@@ -50,15 +70,15 @@ for l = iter
   if( show_fig == 1)
 
     % load results of reconstruction with l iterations
+    %A = load( ['input.dat']);
     A = load( ['output_' num2str( l) '.dat']);
-
+   
     C = reshape( A(:,3), rec_M, rec_M);
     X = reshape( A(:,1), rec_M, rec_M);
     Y = reshape( A(:,2), rec_M, rec_M);
 
     fig0 = figure( 1);
     surfl( X, Y, C);
-
     colormap(bone);                  % set colormap
     shading interp;                  % set shading mode
     
