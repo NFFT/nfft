b4e3ac0665008102c33990f8b92be2f7335da5f3
Jens Keiner <jens@nfft.org>
2005-10-20 22:15:29 +0000

    Bugfixing. Added test data for adjoint NDSFT.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@410 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- tests/lowlevel/nfsft/nfsft_test.c
+++ tests/lowlevel/nfsft/nfsft_test.c
@@ -37,8 +37,11 @@
 /** Maximum filename length */
 #define FILENAME_LENGTH_MAX 50
 
-/** Name of the file containing the test data filenames. */
-const char TESTFILES[] = "ndsft.txt\0";
+/** Name of the file containing the test data filenames for NDSFT. */
+const char TESTFILES_NDSFT[] = "ndsft.txt\0";
+
+/** Name of the file containing the test data filenames for adjoint NDSFT. */
+const char TESTFILES_ADJOINT_NDSFT[] = "adjoint_ndsft.txt\0";
 
 /* Default threshold. Not important here since NDSFT-algorithm is used. */
 #define THRESHOLD 1000.0
@@ -74,7 +77,7 @@ void test_ndsft_trafo(void)
   fprintf(stdout,"ndsft_trafo: Testing ndsft_trafo ...\n");
 
   /* Try to open file containing the names of the test data files. */
-  testfiles = fopen(TESTFILES,"r");
+  testfiles = fopen(TESTFILES_NDSFT,"r");
 
   fprintf(stdout,"%d\n",testfiles);
   fflush(stdout);
@@ -167,6 +170,141 @@ void test_ndsft_trafo(void)
   }  
 }
 
+void test_ndsft_adjoint(void)
+{
+  /** The plan */
+  nfsft_plan plan;
+  /** The bandwidth */
+  int N;
+  /** The number of nodes */
+  int M;
+  /** The original samples */
+  complex* f_hat_orig;
+  /** The degree \f$k\f$ */
+  int k;
+  /** The order \f$n\f$. */
+  int n;
+  /** The node index \f$m\f$*/
+  int m;
+  /** Auxilliary variables used to read in complex numbers. */
+  double d1,d2;
+  /** The file containing the names of the testdata files. */
+  FILE *testfiles;
+  /** The file containg the testcase data */
+  FILE *file;
+  /** Name of file containing test data. */
+  char filename[FILENAME_LENGTH_MAX+1];
+  //char filename[50] = "../../../../../../test.dat";
+  //char filename[50] = "data/test_ndsft_0002_00010.dat";
+
+  /* Tell what we're doing. */
+  fprintf(stdout,"Testing ndsft_adjoint ...\n");
+
+  /* Try to open file containing the names of the test data files. */
+  testfiles = fopen(TESTFILES_ADJOINT_NDSFT,"r");
+
+  fprintf(stdout,"%d\n",testfiles);
+  fflush(stdout);
+
+  /* Test if successful. */
+  if (testfiles == NULL)
+  {
+    CU_FAIL("Couldn't open %s to read test data filenames!\n");      
+    return;
+  }
+
+  while (fscanf(testfiles,"%s",filename) == 1)
+  { 
+    fprintf(stdout,"filename = %s",filename);
+    /* Open input file. */ 
+    file = fopen(filename,"r");
+    /* Check if file was opened successfully. */
+    if (file != NULL)
+    {
+      /* Read in bandwidth. */
+      fscanf(file,"%d",&N);
+      fprintf(stdout,", N = %d",N);
+      /* Read in number of nodes. */
+      fscanf(file,"%d",&M);
+      fprintf(stdout,", M = %d ...",M);
+      /* Precompute. */
+      nfsft_precompute(N,THRESHOLD,0U);
+      /* Initialise plan. */
+      nfsft_init_advanced(&plan,N,M,NFSFT_MALLOC_X | NFSFT_MALLOC_F | 
+        NFSFT_MALLOC_F_HAT | NFSFT_NORMALIZED);
+      /* Read in function samples. */
+      for (m = 0; m < M; m++)
+      {
+        fscanf(file,"%lf",&d1);
+        fscanf(file,"%lf",&d2);
+        plan.f[m] = d1 + I*d2;
+      }
+      /* Read in nodes. */
+      for (m = 0; m < plan.M_total; m++)
+      {
+        fscanf(file,"%lf",&d1);
+        fscanf(file,"%lf",&d2);
+        plan.x[2*m] = d1;
+        plan.x[2*m+1] = d2;
+      }
+      /* Read in reference Fourier coefficients. */
+      f_hat_orig = (complex*) malloc((N+1)*(N+1)*sizeof(complex));
+      for (k = 0; k <= N; k++)
+      {
+        for (n = -k; n <= k; n++)
+        {
+          fscanf(file,"%lf",&d1);
+          fscanf(file,"%lf",&d2);
+          f_hat_orig[k*k+n+k] = d1 + I*d2;
+          //fprintf(stdout,"f_orig[%d] = %lf + I*%lf\n",m,creal(f_orig[m]),cimag(f_orig[m]));
+        }
+      }
+      
+      /* CLose the file. */
+      fclose(file);
+      /* Execute the plan. */
+      ndsft_adjoint(&plan);
+      /* Check result */
+      //fprintf(stdout,"\n");
+      for (k = 0; k <= N; k++)
+      {
+        for (n = -k; n <= k; n++)
+        {
+            /*fprintf(stdout,"f_hat[%d] = %lf + I*%lf, f_hat_orig[%d] = %lf + I*%lf\n",
+              (2*plan.NPT+1)*(n+plan.N)+plan.NPT+k,
+              creal(plan.f_hat[(2*plan.NPT+1)*(n+plan.N)+plan.NPT+k]),
+              cimag(plan.f_hat[(2*plan.NPT+1)*(n+plan.N)+plan.NPT+k]),
+              k*k+n+k,creal(f_hat_orig[k*k+n+k]),cimag(f_hat_orig[k*k+n+k]));*/
+          if (cabs(plan.f_hat[(2*plan.NPT+1)*(n+plan.N)+plan.NPT+k]-
+            f_hat_orig[k*k+n+k]) > 0.0001)
+          {
+            fprintf(stdout," failed\n  f_hat[%d] = %lf + I*%lf, f_hat_orig[%d] = %lf + I*%lf\n",
+              (2*plan.NPT+1)*(n+plan.N)+plan.NPT+k,
+              creal(plan.f_hat[(2*plan.NPT+1)*(n+plan.N)+plan.NPT+k]),
+              cimag(plan.f_hat[(2*plan.NPT+1)*(n+plan.N)+plan.NPT+k]),
+              k*k+n+k,creal(f_hat_orig[k*k+n+k]),cimag(f_hat_orig[k*k+n+k]));
+            CU_FAIL("Wrong result");  
+          }
+        }
+      }    
+      /* Destroy the plan. */
+      nfsft_finalize(&plan);
+      /* Forget precomputed data. */
+      nfsft_forget();
+      /* Free memory. */
+      free(f_hat_orig);
+      /* Test passed. */
+      fprintf(stdout," ok\n");
+      CU_PASS("ok");
+    }
+    else
+    {
+      fprintf(stdout," failed: Couldn't open file %s.\n",filename);
+      CU_FAIL("Couldn't open file!\n");
+    }
+  }  
+}
+
 /**
  * The main program.
  *
@@ -183,6 +321,8 @@ int main (int argc, char **argv)
   CU_pSuite suite = CU_add_suite("NDSFT", NULL, NULL);
   /* Add test for ndsft_trafo. */
   CU_add_test(suite,"NDSFT",test_ndsft_trafo);
+  /* Add test for ndsft_adjoint. */
+  CU_add_test(suite,"adjoint NDSFT",test_ndsft_adjoint);
   /* Run the tests. */
   CU_automated_run_tests();
   /* Cleanup registry. */
