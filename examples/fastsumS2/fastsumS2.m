0b07d0f0d2d9f47e4c39fa2a184c350893db0fba
Jens Keiner <jens@nfft.org>
2006-02-19 22:22:11 +0000

    Continued fastsumS2 example.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@698 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- examples/fastsumS2/fastsumS2.m
+++ examples/fastsumS2/fastsumS2.m
@@ -10,18 +10,25 @@ if (selection == 1)
   parameters = {};
   parameters{1,1,1} = 'h';
   parameters{1,1,2} = 0.8;
-  nodes = {};
-  nodes{1,1} = 1000;
-  nodes{1,2} = 1000;
+  parameters{2,1,1} = 'h';
+  parameters{2,1,2} = 0.9;  nodes = {};
+  nodes{1,1} = 10;
+  nodes{1,2} = 10;
   nodes{1,3} = 1;
   nodes{1,4} = 0;
   nodes{1,5} = 1;
+  nodes{2,1} = 100;
+  nodes{2,2} = 100;
+  nodes{2,3} = 1;
+  nodes{2,4} = 0;
+  nodes{2,5} = 1;
   parameters
   nodes
   writeTestcase(file,0,0,6,1000,0,parameters,4:4:64,nodes);
   fclose(file);
-%  system(sprintf('./%s < %s',programname,infilename));
-%   file = fopen(outfilename,'r');
+  system(sprintf('./%s < %s',programname,infilename));
+  file = fopen(outfilename,'r');
+  T = readTestcase(file)
 %   kernel = fscanf(file,'kernel=%d\n')
 %   nfsft = fscanf(file,'nfsft=%d\n')
 %   if (nfsft == 1)
@@ -31,5 +38,5 @@ if (selection == 1)
 %     end
 %     threshold = fscanf(file,'threshold=%d\n')
 %   end
-%   fclose(file);
+   fclose(file);
 end
