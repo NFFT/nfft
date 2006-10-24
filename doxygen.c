09f11fe212bcc4278f7d93be3753b0a480f1956f
Daniel Potts <potts@mathematik.tu-chemnitz.de>
2006-10-24 14:49:20 +0000

    Einleitung doxygen
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@1133 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- doxygen.c
+++ doxygen.c
@@ -4,7 +4,7 @@
  *
  * Fast Fourier transforms (FFTs) belong to the '10 algorithms with the
  * greatest influence on the development and practice of science and
- * engineering in the 20th century' (J. Dongarra, S. Sullivan).
+ * engineering in the 20th century'.
  * The classic algorithm computes the discrete Fourier transform
  * \f[
  *   f_j= \sum_{k=-\frac{N}{2}}^{\frac{N}{2}-1} \hat{f}_{k}
@@ -25,6 +25,42 @@
  * Fourier transform (NDFT) and its generalisations in one or more dimensions,
  * of arbitrary input size, and of complex data.
  *
- * We believe that our library, which is free software and based on  FFTW3,
- * should become the NFFT library of choice for most applications.
+ * More precisely,we collect the possible frequencies 
+ * \f$\mathbf{k}\in\mathbb{Z}^d\f$ in the multi-index set
+ *\f[
+ * I_{\mathbf{N}} := \left\{ \mathbf{k}=\left(k_t\right)_{t=0,\hdots,d-1} 
+ *  \in \mathbb{Z}^d: -
+ *   \frac{N_t}{2} \le k_t < \frac{N_t}{2} ,\;t=0,\hdots,d-1\right\},
+ *\f]
+ *where \f$\mathbf{N}=\left(N_t\right)_{t=0,\hdots,d-1}\f$ is the
+ * multibandlimit, i.e., \f$N_t\in 2\mathbb{N}\f$.
+ * For a finite number of given Fourier coefficients 
+ * \f$\hat f_{\mathbf{k}} \in \mathbb{C}\f$, 
+ * \f$\mathbf{k}\in I_{\mathbf{N}}\f$ we consider the 
+ * fast evaluation of the trigonometric polynomial 
+ * \f[
+ *  f\left(\mathbf{x}\right) 
+ *  := \sum_{ \mathbf{k}\in I_{ N}} \hat{f}_{\mathbf{ k}} 
+ *  {\rm e}^{-2\pi{\rm i}\mathbf{k}\mathbf{ x}}
+ * \f]
+ * at given nonequispaced nodes \f$ x_j \in \mathbb{T}^d\f$ 
+ * \f$(j=0,\ldots, M-1)\f$ from the 
+ * \f$ d\f$-dimensional torus as well as the 
+ * adjoint problem, the fast evaluation of sums of the form
+ * \f[
+ *  h_{\mathbf{k}} := \sum_{j=0}^{M-1} \hat{f}_{j} 
+ *  {\rm e}^{2\pi{\rm i}\mathbf{k}\mathbf{ x}_j}
+ * \f]
+ *
+ * The generalisations of the NFFT include
+ *    - NNFFT - nonequispaced in time and frequency fast Fourier transform
+ *    - NFCT/NFST - nonequispaced fast (co)sine transform   
+ *    - NSFFT - nonequispaced sparse fast Fourier transform
+ *    - FPT - fast polynomial transform
+ *    - NFSFT - nonequispaced fast spherical Fourier transform
+ *  
+ *
+ *
+ * Furthermore we consider the inversion of the above transform by 
+ *iterative methods.
  */
