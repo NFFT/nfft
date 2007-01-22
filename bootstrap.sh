6bb8f2facfc3ff32f1e02553369ebdc3f95fd3a4
Jens Keiner <jens@nfft.org>
2007-01-22 15:08:52 +0000

    Rewrote bootstrap.sh based on the latest fftw release (3.1.2). Minor changes to AUTHORS. Changed version string in configure.in to 3.0.2. Added an entry to the change log.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@1378 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,20 @@
-# script to initialize automake/autoconf etc.
-autoheader
-aclocal
-automake --add-missing
-autoconf
-libtoolize --automake
-autoheader
-aclocal
-automake --add-missing
-autoconf
-libtoolize --automake
-rm -f config.cache
+#!/bin/sh
+############################################################################ NOTE: If you just want to build NFFT 3, do not use this file. Just follow 
+# the installation instructions as described in the tutorial found under
+# doc/tutorial.
+#
+# This file is based on the bootstrap.sh script from fftw 3.1.2 by 
+# M. Frigo and S. G. Johnson
+###########################################################################
 
+touch ChangeLog
+
+echo "PLEASE IGNORE WARNINGS AND ERRORS"
+
+# paranoia: sometimes autoconf doesn't get things right the first time
+rm -rf autom4te.cache
+autoreconf --verbose --install --symlink --force
+autoreconf --verbose --install --symlink --force
+autoreconf --verbose --install --symlink --force
+
+rm -f config.cache
