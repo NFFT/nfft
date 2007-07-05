93e7a62a0013d46d6249ed30cac1585d4d2f496c
ana <ana>
2007-07-05 09:15:51 +0000

    schmalz: minor changes
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@1705 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- applications/texture/examplesTexture/texture_util/forallhosts.sh
+++ applications/texture/examplesTexture/texture_util/forallhosts.sh
@@ -1,14 +1,26 @@
 hosts=""
+if [ -z "$2" ]
+then
+	delay=5
+else
+	delay=$2
+fi
+if [ -z "$3" ]
+then
+	final=20
+else
+	final=$3
+fi
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14
 do
-hosts="$hosts sfspc$i"
+hosts="$hosts schmalzm@sfspc$i"
 done
 
 for host in $hosts
 do
 ( ssh $host $1 ) &
-sleep 1
+sleep $delay
 done
-sleep 20
+sleep $final
 killall ssh
