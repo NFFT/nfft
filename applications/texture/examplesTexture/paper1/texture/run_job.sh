1857c5c97541bc2e3d625522559448ec789e6305
ana <ana>
2007-07-24 10:16:21 +0000

    schmalz: minor changes
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@1769 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

---
+++ applications/texture/examplesTexture/paper1/texture/run_job.sh
@@ -0,0 +1,24 @@
+jobdir=jobs
+jobName=$1
+lockDir=../../workinghosts
+
+cat $jobdir/running.$jobName | nice ./test >$jobdir/log.$jobName 2>&1
+success=$?
+
+# Ready
+cd $jobdir
+if [ $success -eq 0 ]
+then
+
+rm $lockDir/`hostname`
+mv running.$jobName done.$jobName
+rm log.$jobName
+
+else
+
+rm $lockDir/`hostname`
+echo Job failed!
+mv running.$jobName failed.$jobName
+exit 1
+
+fi
