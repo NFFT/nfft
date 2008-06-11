5378f3e4bf24d7c7b33d0efc959f5e9bbe284732
Jens Keiner <jens@nfft.org>
2008-06-11 08:07:49 +0000

    Added files generating quadrature rules.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@2381 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- matlab/nfsft/gl.m
+++ matlab/nfsft/gl.m
@@ -1,73 +1,103 @@
-function [x,w] = gl(m)
-% Generate Gauss-Legendre nodes and weights on the sphere
+function [x,w] = gl(n)
+%GL  Gauss-Legendre interpolatory quadrature rule.
+%   X = GL(N) generates the (2N+2)*(N+1) Gauss-Legendre nodes and returns a
+%   2x[(2N+2)*(N+1)] matrix X containing their spherical coordinates. The first 
+%   row contains the longitudes in [0,2pi] and the second row the colatitudes in
+%   [0,pi].
 %
-% Copyright 2006,2007 Jens Keiner
-
-% Generate Gauss-Legendre nodes in co-latitudinal direction.
-[theta,w] = lgwt(m+1,-1,1);
-theta = acos(theta);
-phi = 2*pi*(0:2*m+1)/(2*m+2);
-[x,y] = meshgrid(theta,phi);
-x = [y(:),x(:)]';
-w = repmat((pi/(m+1))*w,1,2*m+2)';
-w = w(:);
-
-function [x,w]=lgwt(N,a,b)
-
-% lgwt.m
+%   [X,W] = GL(N) in addition generates the quadrature weights W. The resulting 
+%   quadrature rule is exact up to polynomial degree 2*N.
 %
-% This script is for computing definite integrals using Legendre-Gauss
-% Quadrature. Computes the Legendre-Gauss nodes and weights  on an interval
-% [a,b] with truncation order N
+%   Example:
+%   [X,W] = GL(1) gives
 %
-% Suppose you have a continuous function f(x) which is defined on [a,b]
-% which you can evaluate at any x in [a,b]. Simply evaluate it at all of
-% the values contained in the x vector to obtain a vector f. Then compute
-% the definite integral using sum(f.*w);
+%   X =
+%        0    1.5708    3.1416    4.7124         0    1.5708    3.1416    4.7124
+%   0.9553    0.9553    0.9553    0.9553    2.1863    2.1863    2.1863    2.1863
 %
-% Written by Greg von Winckel - 02/25/2004
-N=N-1;
-N1=N+1; N2=N+2;
+%   W =
+%   1.5708    1.5708    1.5708    1.5708    1.5708    1.5708    1.5708    1.5708
+%
+%   See also cc, healpix, equidist
+%
+%   References
+%   TODO Add references.
+%
+%   Copyright 2006-2007 Jens Keiner
+%   $Id$
 
-xu=linspace(-1,1,N1)';
+%   This program is free software; you can redistribute it and/or modify it 
+%   under the terms of the GNU General Public License as published by the Free 
+%   Software Foundation; either version 2 of the License, or (at your option) 
+%   any later version.
+% 
+%   This program is distributed in the hope that it will be useful, but WITHOUT
+%   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+%   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+%   more details.
+% 
+%   You should have received a copy of the GNU General Public License along with
+%   this program; if not, write to the Free Software Foundation, Inc., 51
+%   Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Initial guess
-y=cos((2*(0:N)'+1)*pi/(2*N+2))+(0.27/N1)*sin(pi*xu*N/N2);
-
-% Legendre-Gauss Vandermonde Matrix
-L=zeros(N1,N2);
-
-% Derivative of LGVM
-Lp=zeros(N1,N2);
-
-% Compute the zeros of the N+1 Legendre Polynomial
-% using the recursion relation and the Newton-Raphson method
-
-y0=2;
-
-% Iterate until new points are uniformly within epsilon of old points
-while max(abs(y-y0))>eps
-
-
-    L(:,1)=1;
-    Lp(:,1)=0;
-
-    L(:,2)=y;
-    Lp(:,2)=1;
-
-    for k=2:N1
-        L(:,k+1)=( (2*k-1)*y.*L(:,k)-(k-1)*L(:,k-1) )/k;
-    end
-
-    Lp=(N2)*( L(:,N1)-y.*L(:,N2) )./(1-y.^2);
-
-    y0=y;
-    y=y0-L(:,N2)./Lp;
-
+% correctness conditions
+if (nargin ~= 1)
+  error('gl: Exactly one input argument required.');
+end
+if (~isscalar(n) || n < 0)
+  error('gl: Input argument must be a non-negative number.');
+end
+if (nargout > 2)
+  error('gl: No more than two output arguments allowed.');
 end
 
-% Linear map from[-1,1] to [a,b]
-x=(a*(1-y)+b*(1+y))/2;
+% Gauss-Legendre nodes and weights (maybe) on [-1,1]
+if (nargout == 2)
+  [theta,w] = lgwt(n+1,-1,1);
+else
+  theta = lgwt(n+1,-1,1);
+end  
+% coordinate transformation to [0,pi] for colatitude
+theta = acos(theta);
+% equispaced nodes for longitude
+phi = 2*pi*(0:2*n+1)/(2*n+2);
+% tensor product
+[x,y] = meshgrid(theta,phi);
+% linearisation to 2x((2*n+2)(n+1)) matrix
+x = [y(:),x(:)]';
+if (nargout == 2)
+  % weights
+  w = repmat((pi/(n+1))*w,1,2*n+2)';
+  w = w(:)';
+end
 
-% Compute the weights
-w=(b-a)./((1-y.^2).*Lp.^2)*(N2/N1)^2;
+% The following function is based on code by Greg von Winckel, 02/25/2004
+% See: TODO Add references.
+function [x,w] = lgwt(n,a,b)
+
+n = n-1; n1= n + 1; n2 = n + 2;
+% n1 uniform nodes in [-1,1]
+xu = linspace(-1,1,n1)';
+% initial guess for nodes
+y=cos((2*(0:n)'+1)*pi/(2*n+2)) + (0.27/n1)*sin(pi*xu*n/n2);
+% Gauss-Legendre Vandermonde matrix
+L=zeros(n1,n2);
+% derivative of that
+Lp=zeros(n1,n2);
+% We compute the zeros of the n1th Legendre polynomial using the recursion 
+% relation and the Newton-Raphson method.
+y0=2;
+% Iterate until new points are uniformly within epsilon of old points.
+while (max(abs(y - y0)) > eps)
+  L(:,1) = 1; Lp(:,1) = 0; L(:,2) = y; Lp(:,2) = 1;
+  for k = 2:n1
+    L(:,k+1) = ((2*k-1)*y.*L(:,k)-(k-1)*L(:,k-1))/k;
+  end
+  Lp = n2 * (L(:,n1) - y.*L(:,n2))./(1-y.^2); y0 = y; y = y0 - L(:,n2)./Lp;
+end
+% linear map from[-1,1] to [a,b]
+x = (a*(1-y)+b*(1+y))/2;
+if (nargout == 2)
+  % weights
+  w = (b-a)./((1-y.^2).*Lp.^2)*(n2/n1)^2;
+end
