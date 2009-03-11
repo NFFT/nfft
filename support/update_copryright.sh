0a0555b7f0f7ea0e667fc0c77599645bf500c54c
Jens Keiner <jens@nfft.org>
2009-03-11 12:01:03 +0000

    Minor tweaks.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@3092 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- support/update_copryright.sh
+++ support/update_copryright.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-for name in $(find .. -name "*.[ch]"); do
-  sed "1,/^\/\* \$Id/d" < $name > $name.tmp
-  if test "x$(stat -c%s $name.tmp)" = "x0"; then
-    echo "Warning: $name does not seem to have the correct header format."
+function replace {
+  sed "1,/^\/\* \$Id/d" < $1 > $1.tmp
+  if test "x$(stat -c%s $1.tmp)" = "x0"; then
+    echo "Warning: $1 does not seem to have the correct header format."
   else
-    cat copyright.txt $name.tmp > $name
+    cat copyright.txt $1.tmp > $1
   fi
-  rm -f $name.tmp
+  rm -f $1.tmp
+}
+
+for name in $(find .. -wholename "../applications/texture" -prune -o -name "*.[ch]" -print -o -name "nfftconf.h.in" -print); do
+  replace $name
 done
 
