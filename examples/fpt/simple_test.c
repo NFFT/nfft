6566369a59abc57118fefaaa6dd79f37154b46b3
Jens Keiner <jens@nfft.org>
2009-04-07 11:05:56 +0000

    Changed fpt example program so that it does not use the NFCT anymore.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/trunk@3153 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- examples/fpt/simple_test.c
+++ examples/fpt/simple_test.c
@@ -44,13 +44,13 @@
 
 int main(void)
 {
-  /* This example shows how to use the fast polynomial transform to evaluate a
+  /* This example shows the use of the fast polynomial transform to evaluate a
    * finite expansion in Legendre polynomials,
    *
    *   f(x) = a_0 P_0(x) + a_1 P_1(x) + ... + a_N P_N(x)                     (1)
    *
-   * at a number of M arbitrary nodes x_j in [-1,1]. */
-  const int N = 8, M = 10;
+   * at the Chebyshev nodes x_j = cos(j*pi/N), j=0,1,...,N. */
+  const int N = 8;
 
   /* An fpt_set is a data structure that contains precomputed data for a number
    * of different polynomial transforms. Here, we need only one transform. the
@@ -60,11 +60,12 @@ int main(void)
 
   /* Three-term recurrence coefficients for Legendre polynomials */
   double *alpha = malloc((N+2)*sizeof(double)),
-    *beta = malloc((N+2)*sizeof(double)), *gamma = malloc((N+2)*sizeof(double));
+    *beta = malloc((N+2)*sizeof(double)),
+    *gamma = malloc((N+2)*sizeof(double));
 
   /* alpha[0] and beta[0] are not referenced. */
   alpha[0] = beta[0] = 0.0;
-  /* gamma[0] contains P_0(x) which is a constant. */
+  /* gamma[0] contains the value of P_0(x) (which is a constant). */
   gamma[0] = 1.0;
 
   /* Actual three-term recurrence coefficients for Legendre polynomials */
@@ -79,13 +80,12 @@ int main(void)
   }
 
   printf(
-    "Computing a fast polynomial transform (FPT) and a fast nonequispaced \n"
-    "cosine transform (NFCT) to evaluate\n\n"
-    "  f_j = a_0 P_0(x) + a_1 P_1(x) + ... + a_N P_N(x), j=1,2,...,M,\n\n"
-    "with N=%d, M=%d, x_j random nodes in [-1,1], and a_k, k=0,1,...,N random\n"
-    "Fourier coefficients in [-1,1], where P_k, k=0,1,...,N are the Legendre "
-    "polynomials.",
-    N,M
+    "Computing a fast polynomial transform (FPT) and a fast discrete cosine \n"
+    "transform (DCT) to evaluate\n\n"
+    "  f_j = a_0 P_0(x_j) + a_1 P_1(x_j) + ... + a_N P_N(x_j), j=0,1,...,N,\n\n"
+    "with N=%d, x_j = cos(j*pi/N), j=0,1,...N, the Chebyshev nodes, a_k,\n"
+    "k=0,1,...,N, random Fourier coefficients in [-1,1]x[-1,1]*I, and P_k,\n"
+    "k=0,1,...,N, the Legendre polynomials.",N
   );
 
   /* Random seed, makes things reproducible. */
@@ -97,73 +97,63 @@ int main(void)
    * is explained above. The sixth parameter (k_start) is where the index in the
    * linear combination (1) starts, here k_start=0. The seventh parameter
    * (kappa) is the threshold which has an influence on the accuracy of the fast
-   * polynomial transform. Usually, kappa=1000 is a good choice. */
+   * polynomial transform. Usually, kappa = 1000 is a good choice. */
   fpt_precompute(set,0,alpha,beta,gamma,0,1000.0);
 
+
   {
-    /* Plan for NFCT */
-    nfct_plan p;
+    /* Arrays for Fourier coefficients and function values. */
+    double _Complex *a = malloc((N+1)*sizeof(double _Complex));
+    double _Complex *b = malloc((N+1)*sizeof(double _Complex));
+    double *f = malloc((N+1)*sizeof(double _Complex));
 
-    /* Init transform plan. */
-    nfct_init_1d(&p, N+1, M);
+    /* Plan for discrete cosine transform */
+    const int NP1 = N + 1;
+    fftw_r2r_kind kind = FFTW_REDFT00;
+    fftw_plan p = fftw_plan_many_r2r(1, &NP1, 1, (double*)b, NULL, 2, 1,
+      (double*)f, NULL, 1, 1, &kind, 0U);
 
-    /* random nodes */
+    /* random Fourier coefficients */
     {
-      int j;
-      printf("\n1) Random nodes x_j, j=1,2,...,M:\n");
-      for (j = 0; j < M; j++)
+      int k;
+      printf("\n2) Random Fourier coefficients a_k, k=0,1,...,N:\n");
+      for (k = 0; k <= N; k++)
       {
-        p.x[j] = acos(2.0*drand48() - 1.0)/KPI2;
-        /* for debugging: use acos(2.0*((double)j)/((double)(M-1))-1.0)/KPI2 */
-        printf("   x_%-2d = %+5.3lE\n",j+1,cos(KPI2*p.x[j]));
+        a[k] = 2.0*drand48() - 1.0; /* for debugging: use k+1 */
+        printf("   a_%-2d = %+5.3lE\n",k,creal(a[k]));
       }
     }
 
-    if(p.nfct_flags & PRE_PSI)
-      nfct_precompute_psi(&p);
+    /* fast polynomial transform */
+    fpt_trafo(set,0,a,b,N,0U);
+
+    /* Renormalize coefficients b_j, j=1,2,...,N-1 owing to how FFTW defines a
+     * DCT-I; see
+     * http://www.fftw.org/fftw3_doc/1d-Real_002deven-DFTs-_0028DCTs_0029.html
+     * for details */
+    {
+      int j;
+      for (j = 1; j < N; j++)
+        b[j] *= 0.5;
+    }
+
+    /* discrete cosine transform */
+    fftw_execute(p);
 
     {
-      /* Array for Fourier coefficients. */
-      double _Complex *a = malloc((N+1)*sizeof(double _Complex));
-      double _Complex *b = malloc((N+1)*sizeof(double _Complex));
-
-      /* random Fourier coefficients */
-      {
-        int k;
-        printf("\n2) Random Fourier coefficients a_k, k=0,1,...,N:\n");
-        for (k = 0; k <= N; k++)
-        {
-          a[k] = 2.0*drand48() - 1.0; /* for debugging: use k+1 */
-          printf("   a_%-2d = %+5.3lE\n",k,creal(a[k]));
-        }
-      }
-
-      /* fast polynomial transform */
-      fpt_trafo(set,0,a,b,N,0U);
-
-      {
-        int k;
-        for (k = 0; k <= N; k++)
-          p.f_hat[k] = creal(b[k]);
-      }
-
-      /* nonequispaced discrete cosine transform */
-      nfct_trafo(&p);
-
-      {
-        int j;
-        printf("\n3) Function values f_j, j=1,1,...,M:\n");
-        for (j = 0; j < M; j++)
-          printf("   f_%-2d = %+5.3lE\n",j+1,p.f[j]);
-      }
-
-      /* cleanup */
-      free(a);
-      free(b);
+      int j;
+      printf("\n3) Function values f_j, j=1,1,...,M:\n");
+      for (j = 0; j <= N; j++)
+        printf("   f_%-2d = %+5.3lE\n",j,f[j]);
     }
 
     /* cleanup */
-    nfct_finalize(&p);
+    free(a);
+    free(b);
+    free(f);
+
+    /* cleanup */
+    fftw_destroy_plan(p);
   }
 
   /* cleanup */
