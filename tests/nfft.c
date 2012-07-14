5a7b7691bf349c4b1a4a70cace1aad8b13156891
Jens Keiner <jens@nfft.org>
2012-07-14 12:42:26 +0000

    Debugging JUnit parsing.
    git-svn-id: https://euler.mathematik.tu-chemnitz.de/repos/nfft/lib/branches/cunit_tests@3835 c8b3e4ef-9df4-0310-9d76-b6dd82e1323a

--- tests/nfft.c
+++ tests/nfft.c
@@ -800,11 +800,11 @@ static const init_delegate_t* initializers_4d[] =
 #endif
 };
 
-static const testcase_delegate_online_t nfft_online_4d_36_50 = {X(setup_online), X(destroy_online), 4, 28 ,50};
+static const testcase_delegate_online_t nfft_online_4d_28_50 = {X(setup_online), X(destroy_online), 4, 28 ,50};
 
 static const testcase_delegate_online_t *testcases_4d_online[] =
 {
-  &nfft_online_4d_36_50,
+  &nfft_online_4d_28_50,
 };
 
 static const trafo_delegate_t* trafos_4d_online[] = {&trafo};
