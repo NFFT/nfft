3cd03028a1f6695f19438ebca2edeac8d64a6e64
Toni Volkmer <tvolkmer@users.noreply.github.com>
2018-02-01 10:56:07 +0100

    Feature/octave tests (#65)
    * ported CUnit-like tests for nfftmex
    
    * add output of failed tests
    
    * use sudo and newer octave for matlab interface tests
    
    * always show cpuinfo
    
    * slightly increased test threshold for bessel_i0 in single precision:
    'random' failures of make check occurred on travis-ci when using clang 5.0.0 and single precision. Only one single check slightly failed:
    i0[ 1.50000000E+01] =  3.39648438E+05 err_rel =  2.76019932E-06 >=  1.90734863E-06 -> FAIL
    This check only seems to fail when the used cpufamily/model is 6/63 on but pass for 6/45 and 6/62. Presumably, the main difference is that 6/63 supports avx2. On a computer with clang 3.7.0 and cpu 6/94 with avx2, the identical error value 2.76019932E-06 is obtained.
    When using gcc, the error value is just slightly below the threshold.
    I have increased the threshold to 2.861E-06 (24*eps instead of 16*eps) for now.
    
    * use MATLAB / Octave path from configure script
    
    * change matlab call compatible to windows and macos
    
    * add ABS_SRCDIR and more error handling to matlab/octave tests

---
+++ matlab/tests/nfftUnitTestsRunAndExit.m
@@ -0,0 +1,50 @@
+clear all;
+addpath ../nfft
+ok = 1;
+
+try
+  tests = nfftUnitTests;
+  
+  result = nfft_check_1d_direct_file(tests); ok = min(ok, result);
+  result = nfft_check_1d_fast_file(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_1d_direct_file(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_1d_fast_file(tests); ok = min(ok, result);
+  result = nfft_check_1d_online(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_1d_online(tests); ok = min(ok, result);
+  
+  result = nfft_check_2d_direct_file(tests); ok = min(ok, result);
+  result = nfft_check_2d_fast_file(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_2d_direct_file(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_2d_fast_file(tests); ok = min(ok, result);
+  result = nfft_check_2d_online(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_2d_online(tests); ok = min(ok, result);
+  
+  result = nfft_check_3d_direct_file(tests); ok = min(ok, result);
+  result = nfft_check_3d_fast_file(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_3d_direct_file(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_3d_fast_file(tests); ok = min(ok, result);
+  result = nfft_check_3d_online(tests); ok = min(ok, result);
+  result = nfft_check_adjoint_3d_online(tests); ok = min(ok, result);
+
+%  result = nfft_check_4d_online(tests); ok = min(ok, result);
+%  result = nfft_check_adjoint_4d_online(tests); ok = min(ok, result);
+
+  clear tests;
+catch err
+  try
+    fprintf('Exception %s %s\n', err.identifier, err.message);
+    err
+  catch
+  end
+  ok = 0;
+end
+
+clear result;
+
+if ok ~= 1
+  fprintf('nfftUnitTest: at least one test failed\n');
+  exit(1);
+  return;
+end
+fprintf('nfftUnitTest: all tests succeeded\n');
+exit(0);
