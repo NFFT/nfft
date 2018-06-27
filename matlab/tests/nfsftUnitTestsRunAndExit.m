151bc68d73c57fe3f6de682008548a41f5bd00c3
Michael Quellmalz <michael.quellmalz@mathematik.tu-chemnitz.de>
2018-06-27 20:40:19 +0200

    Fixes for NFSOFT unit tests in Octave
    The Octave built-in legendre function is very very slow

--- matlab/tests/nfsftUnitTestsRunAndExit.m
+++ matlab/tests/nfsftUnitTestsRunAndExit.m
@@ -6,16 +6,16 @@ bound_direct = 2^12* eps;
 bound_reconstruction = 2^22* eps;
 
 try
-  % Comparison with accurate (slow) algorithm from Matlab
   for N = 2.^(2:10)
     % precomputation
     nfsft_precompute(N,1000);
 
+    % Comparison with accurate (slow) algorithm from Matlab
     % random Fourier coefficients
     fh = f_hat(rand((N+1)*(N+1),1)./(1:(N+1)*(N+1))');
 
     % number of nodes
-    M = 100;
+    M = 80;
 
     % random nodes
     ph=rand(1,M)*2*pi;
@@ -74,39 +74,63 @@ try
 
 
     % Algorithm in Matlab
-    f_mat = legendre(0,cos(X(2,:)),'norm').' / sqrt(2*pi) * fh(0,0);
-    fh2_mat = zeros(size(fh.f_hat));
-    fh2_mat(1)= legendre(0,cos(X(2,:)),'norm') / sqrt(2*pi) * f;
-    for n=1:N
-      Lp = legendre(n,cos(X(2,:)),'norm') / sqrt(2*pi);
-      f_mat=f_mat + (flipud(Lp).* exp((-n:0)'.*1i.*X(1,:))).'* fh(n,-n:0)...
-       + (Lp(2:end,:).* exp((1:n)'.*1i.*X(1,:))).'* fh(n,1:n);
+    % this is very slow in Octave
+    if N<200 || ~exist('octave_config_info')
+      f_mat = legendre(0,cos(X(2,:)),'norm').' / sqrt(2*pi) * fh(0,0);
+      fh2_mat = zeros(size(fh.f_hat));
+      fh2_mat(1)= legendre(0,cos(X(2,:)),'norm') / sqrt(2*pi) * f;
+      for n=1:N
+        Lp = legendre(n,cos(X(2,:)),'norm') / sqrt(2*pi);
+        f_mat=f_mat + (flipud(Lp).* exp((-n:0)'.*1i.*X(1,:))).'* fh(n,-n:0)...
+         + (Lp(2:end,:).* exp((1:n)'.*1i.*X(1,:))).'* fh(n,1:n);
 
-      fh2_mat(n^2+1:n^2+n+1)= (flipud(Lp).* exp(-(-n:0)'.*1i.*X(1,:)))* f;
-      fh2_mat(n^2+n+2:n^2+2*n+1)= (Lp(2:end,:).* exp(-(1:n)'.*1i.*X(1,:)))* f;
-    end
-    fh2_mat = f_hat(fh2_mat);
+        fh2_mat(n^2+1:n^2+n+1)= (flipud(Lp).* exp(-(-n:0)'.*1i.*X(1,:)))* f;
+        fh2_mat(n^2+n+2:n^2+2*n+1)= (Lp(2:end,:).* exp(-(1:n)'.*1i.*X(1,:)))* f;
+      end
+      fh2_mat = f_hat(fh2_mat);
 
-    % Compute error
-    error_trafo = [norm(f_fast-f_mat), norm(f_dpt-f_mat), norm(f_direct-f_mat)]/norm(f_mat);
-    if (max(error_trafo) > bound) || (error_trafo(3) > bound_direct)
-      ok = 0;
-      result = 'FAIL';
-    else
-      result = ' OK ';
-    end
-    fprintf('NFSFT_trafo    (N =%4d) ---> %s  fpt: %.3e, dpt: %.3e (%.3e), direct: %.3e (%.3e)\n', ...
-      N, result, error_trafo(1), error_trafo(2), bound, error_trafo(3), bound_direct);
+      % Compute error
+      error_trafo = [norm(f_fast-f_mat), norm(f_dpt-f_mat), norm(f_direct-f_mat)]/norm(f_mat);
+      if (max(error_trafo) > bound) || (error_trafo(3) > bound_direct)
+        ok = 0;
+        result = 'FAIL';
+      else
+        result = ' OK ';
+      end
+      fprintf('NFSFT_trafo    (N =%4d) ---> %s  fpt: %.3e, dpt: %.3e (%.3e), direct: %.3e (%.3e)\n', ...
+        N, result, error_trafo(1), error_trafo(2), bound, error_trafo(3), bound_direct);
 
-    error_adjoint = [norm(fh2_fast-fh2_mat), norm(fh2_dpt-fh2_mat), norm(fh2_direct-fh2_mat)]/norm(fh2_mat);
-    if (max(error_adjoint) > bound) || (error_adjoint(3) > bound_direct)
-      ok = 0;
-      result = 'FAIL';
-    else
-      result = ' OK ';
+      error_adjoint = [norm(fh2_fast-fh2_mat), norm(fh2_dpt-fh2_mat), norm(fh2_direct-fh2_mat)]/norm(fh2_mat);
+      if (max(error_adjoint) > bound) || (error_adjoint(3) > bound_direct)
+        ok = 0;
+        result = 'FAIL';
+      else
+        result = ' OK ';
+      end
+      fprintf('NFSFT_adjoint  (N =%4d) ---> %s  fpt: %.3e, dpt: %.3e (%.3e), direct: %.3e (%.3e)\n', ...
+        N, result, error_adjoint(1), error_adjoint(2), bound, error_adjoint(3), bound_direct);
+    
+    else %Octave for high N
+      error_trafo = [norm(f_fast-f_direct), norm(f_dpt-f_direct)]/norm(f_direct);
+      if (max(error_trafo) > bound)
+        ok = 0;
+        result = 'FAIL';
+      else
+        result = ' OK ';
+      end
+      fprintf('NFSFT_trafo    (N =%4d) ---> %s  fpt: %.3e, dpt: %.3e (%.3e)\n', ...
+        N, result, error_trafo(1), error_trafo(2), bound);
+
+      error_adjoint = [norm(fh2_fast-fh2_direct), norm(fh2_dpt-fh2_direct)]/norm(fh2_direct);
+      if (max(error_adjoint) > bound) 
+        ok = 0;
+        result = 'FAIL';
+      else
+        result = ' OK ';
+      end
+      fprintf('NFSFT_adjoint  (N =%4d) ---> %s  fpt: %.3e, dpt: %.3e (%.3e)\n', ...
+        N, result, error_adjoint(1), error_adjoint(2), bound);
     end
-    fprintf('NFSFT_adjoint  (N =%4d) ---> %s  fpt: %.3e, dpt: %.3e (%.3e), direct: %.3e (%.3e)\n', ...
-      N, result, error_adjoint(1), error_adjoint(2), bound, error_adjoint(3), bound_direct);
 
     % Run test with many nodes and quadrature
     [X,W] = gl(N);
@@ -114,25 +138,28 @@ try
 
     for use_dpt = 1:2
       % Create plan of class NFSFT.
-      plan = nfsft(N,M,NFSFT_NORMALIZED + (use_dpt-1)*NFSFT_USE_DPT);
-      plan.x = X;
+      plan = nfsft_init_advanced(N,M,NFSFT_NORMALIZED + (use_dpt-1)*NFSFT_USE_DPT);
+      nfsft_set_x(plan,X);
+      nfsft_precompute_x(plan)
 
       % random Fourier coefficients
       fh = f_hat(rand((N+1)*(N+1),1));
-      plan.fhat = fh;
+      nfsft_set_f_hat(plan,double(fh));
 
       % NFSFT transform
       nfsft_trafo(plan);
 
       % function values
-      f = plan.f;
+      f = nfsft_get_f(plan);
 
       % adjoint transform, using quadrature weights to recover Fourier coefficients
       % the adjoint is only the inverse with the right quadrature weights W
-      plan.f = f.*W';
+      nfsft_set_f(plan,f.*W');
       nfsft_adjoint(plan);
 
-      error_quad(use_dpt) = norm(plan.fhat-fh)/norm(fh);
+      fh2 = nfsft_get_f_hat(plan);
+      nfsft_finalize(plan)
+      error_quad(use_dpt) = norm(fh2-fh)/norm(fh);
     end
 
     if max(error_quad) > bound_reconstruction
