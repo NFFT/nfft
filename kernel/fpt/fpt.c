NFFT/nfft

commit 57bef6363e8bd504fc12f1020b3aaf54be55ab3d

#72 minor fix in fpt.c and nfsft unit tests

kernel/fpt/fpt.c
@@ -734,7 +734,7 @@ static inline void eval_sum_clenshaw_fast(const int N, const int M,
       double _Complex tmp1 = a[N-1];
       double _Complex tmp2 = a[N];
       /* 1e247 should not trigger for NFSFT with N <= 1024 */
-      for (k = N-1; k > 0 && fabs(tmp2) < 1e247; k--)
+      for (k = N-1; k > 0 && cabs(tmp2) < 1e247; k--)
       {
         double _Complex tmp3 = a[k-1] + tmp2 * gam[k];
         tmp2 *= (alpha[k] * xc + beta[k]);

matlab/tests/nfsftTestcaseInitDelegate.m
@@ -59,7 +59,7 @@ classdef nfsftTestcaseInitDelegate
         case 'init_class'
           if isempty(h.flags) && isempty(h.nfft_cutoff_m) && isempty(h.fpt_flags) && isempty(h.nfft_flags)
             plan = nfsft(N,M);
-          elseif isempty(h.flags)
+          elseif ~isempty(h.flags)
             plan = nfsft(N,M,h.flags);
           else
             plan = nfsft(N,M,h.flags,h.kappa,h.nfft_cutoff_m,h.fpt_flags,h.nfft_flags);

matlab/tests/nfsftUnitTests.m.in
@@ -13,12 +13,12 @@ classdef nfsftUnitTests
       nfsftTestcaseDelegateOnline(64, 100, 'trafo') ...
       nfsftTestcaseDelegateOnline(128, 10, 'trafo') ...
       nfsftTestcaseDelegateOnline(128, 100, 'trafo') ...
-      nfsftTestcaseDelegateOnline(128, 1000, 'trafo') ...
       nfsftTestcaseDelegateOnline(256, 10, 'trafo') ...
       nfsftTestcaseDelegateOnline(256, 100, 'trafo') ...
                              };
 
     testcases_online_trafo_exhaustive = { ...
+      nfsftTestcaseDelegateOnline(128, 1000, 'trafo') ...
       nfsftTestcaseDelegateOnline(256, 1000, 'trafo') ...
       nfsftTestcaseDelegateOnline(512, 10, 'trafo') ...
       nfsftTestcaseDelegateOnline(512, 100, 'trafo') ...
@@ -34,12 +34,12 @@ classdef nfsftUnitTests
       nfsftTestcaseDelegateOnline(64, 100, 'adjoint') ...
       nfsftTestcaseDelegateOnline(128, 10, 'adjoint') ...
       nfsftTestcaseDelegateOnline(128, 100, 'adjoint') ...
-      nfsftTestcaseDelegateOnline(128, 1000, 'adjoint') ...
       nfsftTestcaseDelegateOnline(256, 10, 'adjoint') ...
       nfsftTestcaseDelegateOnline(256, 100, 'adjoint') ...
                        };
 
     testcases_online_adjoint_exhaustive = { ...
+      nfsftTestcaseDelegateOnline(128, 1000, 'adjoint') ...
       nfsftTestcaseDelegateOnline(256, 1000, 'adjoint') ...
       nfsftTestcaseDelegateOnline(512, 10, 'adjoint') ...
       nfsftTestcaseDelegateOnline(512, 100, 'adjoint') ...
@@ -64,6 +64,9 @@ classdef nfsftUnitTests
       nfsftTestcaseDelegateGLQuadratureOnline(128) ...
       nfsftTestcaseDelegateGLQuadratureOnline(256) ...
       nfsftTestcaseDelegateGLQuadratureOnline(512) ...
+    };
+
+    testcases_gl_quadrature_online_large = { ...
       nfsftTestcaseDelegateGLQuadratureOnline(1024) ...
     };
 
@@ -76,6 +79,10 @@ classdef nfsftUnitTests
       nfsftTestcaseInitDelegate('init_advanced',bitor(NFSFT_NORMALIZED,NFSFT_USE_DPT)) ...
       nfsftTestcaseInitDelegate('init_class',NFSFT_NORMALIZED) ...
       nfsftTestcaseInitDelegate('init_class',bitor(NFSFT_NORMALIZED,NFSFT_USE_DPT)) ...
+    };
+
+    initializers_quadrature_large = { ...
+      nfsftTestcaseInitDelegate('init_class',NFSFT_NORMALIZED) ...
     };
   end
   
@@ -125,8 +132,8 @@ classdef nfsftUnitTests
         end
       end
 
-      init_name = init_delegate.name;
-      fprintf(', %-18s', init_name);
+      init_txt = init_delegate.txt;
+      fprintf(', %-18s', init_txt);
       [init_delegate, plan] = init_delegate.init(testcase.N, testcase.M);
 
       if ~isempty(init_delegate.nfft_cutoff_m)
@@ -293,6 +300,10 @@ classdef nfsftUnitTests
 
     function result = nfsft_check_quadrature_online(h)
       result = h.check_many_quadrature(h.testcases_gl_quadrature_online, h.initializers_quadrature, {nfsftTestcaseTrafoDelegate('adjoint')});
+
+      result2 = h.check_many_quadrature(h.testcases_gl_quadrature_online_large, h.initializers_quadrature_large, {nfsftTestcaseTrafoDelegate('adjoint')});
+      result = min(result,result2);
+
       if h.perform_exhaustive_tests_flag
         result2 = h.check_many_quadrature(h.testcases_gl_quadrature_online_exhaustive, h.initializers_quadrature, {nfsftTestcaseTrafoDelegate('adjoint')});
         result = min(result,result2);

commit a063666d42424979ed7cd4f61281a8a5816a94c9

#72 fix error in fpt.c introduced in commit 57bef63, which causes nan in DPT

kernel/fpt/fpt.c
@@ -734,7 +734,7 @@ static inline void eval_sum_clenshaw_fast(const int N, const int M,
       double _Complex tmp1 = a[N-1];
       double _Complex tmp2 = a[N];
       /* 1e247 should not trigger for NFSFT with N <= 1024 */
-      for (k = N-1; k > 0 && cabs(tmp2) < 1e247; k--)
+      for (k = N-1; k > 0 && fabs(creal(tmp2)) < 1e247 && fabs(cimag(tmp2)) < 1e247; k--)
       {
         double _Complex tmp3 = a[k-1] + tmp2 * gam[k];
         tmp2 *= (alpha[k] * xc + beta[k]);