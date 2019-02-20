73ebbc78eda6c73305b93a78bad546bf11e5fee6
melaniekircheis <melanie.kircheis@s2013.tu-chemnitz.de>
2019-02-20 16:19:10 +0100

    Renamed variable in test_underdetermined

--- matlab/infft1d/test_underdetermined.m
+++ matlab/infft1d/test_underdetermined.m
@@ -34,7 +34,7 @@ infft_direct(plan); % Compute samples directly
 
 % Graphical representation of the pointwise reconstruction
 figure(1)
-plot(-N/2:N/2-1,coeffs,'o',-N/2:N/2-1,real(plan.fcheck),'*')
+plot(-N/2:N/2-1,fhat,'o',-N/2:N/2-1,real(plan.fcheck),'*')
 title('Pointwise reconstruction')
 xlabel('$k$','Interpreter','latex')
 ylabel('Fourier coefficients $\hat f_k$','Interpreter','latex')
@@ -43,7 +43,7 @@ xlim([-N/2-1,N/2])
 
 % Graphical representation of pointwise errors
 figure(2)
-semilogy(-N/2:N/2-1,abs(plan.fcheck(:)-coeffs(:)),'-sg',-N/2:N/2-1,abs(plan.fcheck(:)-coeffs(:))./norm(coeffs,Inf),'-dr')
+semilogy(-N/2:N/2-1,abs(plan.fcheck(:)-fhat(:)),'-sg',-N/2:N/2-1,abs(plan.fcheck(:)-fhat(:))./norm(fhat,Inf),'-dr')
 title('Pointwise maximum errors')
 xlabel('$k$','Interpreter','latex')
 ylabel('pointwise errors')
@@ -51,10 +51,10 @@ legend('absolute maximum error','relative maximum error','Location','best')
 xlim([-N/2-1,N/2])
 
 % Computation of errors
-err_abs_max = norm(plan.fcheck(:)-coeffs(:),Inf);                  	% Absolute ell_infinity error
-err_rel_max = norm(plan.fcheck(:)-coeffs(:),Inf)/norm(coeffs,Inf);	% Relative ell_infinity error
-err_abs_2 = norm(plan.fcheck(:)-coeffs(:),2);                       % Absolute ell_2 error
-err_rel_2 = norm(plan.fcheck(:)-coeffs(:),2)/norm(coeffs,2);        % Relative ell_2 error
+err_abs_max = norm(plan.fcheck(:)-fhat(:),Inf);                 % Absolute ell_infinity error
+err_rel_max = norm(plan.fcheck(:)-fhat(:),Inf)/norm(fhat,Inf);	% Relative ell_infinity error
+err_abs_2 = norm(plan.fcheck(:)-fhat(:),2);                     % Absolute ell_2 error
+err_rel_2 = norm(plan.fcheck(:)-fhat(:),2)/norm(fhat,2);        % Relative ell_2 error
 
 % Output in command window
 fprintf(['The absolute maximum error is ',num2str(err_abs_max,'%1.4e'),' and the relative maximum error is ',num2str(err_rel_max,'%1.4e'),'.\n\n'])
