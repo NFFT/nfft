1a221cd121ad16abc62958f4b1291ef834bc7284
Michael Schmischke <michael.schmischke@math.tu-chemnitz.de>
2020-08-25 12:30:49 +0200

    Update NFST.jl
    remove info

--- julia/nfst/NFST.jl
+++ julia/nfst/NFST.jl
@@ -97,8 +97,6 @@ function NFSTplan(N::NTuple{D,Integer},M::Integer) where {D}
 end
 
 function NFSTplan(N::NTuple{D,Integer},M::Integer,n::NTuple{D,Integer},m::Integer=Int32(8),f1::UInt32=(D > 1 ? f1_default : f1_default_1d),f2::UInt32=f2_default) where {D}
-	@info "You are using the guru interface. Please consult the README if you are having trouble."
-
     # safety checks
 	if any(x->x<=0,N)
 		error("Every entry of N has to be an even, positive integer." )
